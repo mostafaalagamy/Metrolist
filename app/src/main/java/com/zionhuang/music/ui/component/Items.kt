b103664b3ee10447ef4d26d1f3e3964fa94d1b7e
Malopieds <malo.beauchamps@proton.me>
2024-03-22 22:55:54 +0100

    feat(ui): active song background color change

--- app/src/main/java/com/zionhuang/music/ui/component/Items.kt
+++ app/src/main/java/com/zionhuang/music/ui/component/Items.kt
@@ -102,12 +102,20 @@ inline fun ListItem(
     noinline subtitle: (@Composable RowScope.() -> Unit)? = null,
     thumbnailContent: @Composable () -> Unit,
     trailingContent: @Composable RowScope.() -> Unit = {},
+    isActive: Boolean = false,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
+        modifier = if (isActive)
+                modifier
+                    .height(ListItemHeight)
+                    .padding(horizontal = 8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = MaterialTheme.colorScheme.secondaryContainer)
+            else
+                modifier
             .height(ListItemHeight)
-            .padding(horizontal = 6.dp),
+            .padding(horizontal = 8.dp)
     ) {
         Box(
             modifier = Modifier.padding(6.dp),
@@ -147,6 +155,7 @@ fun ListItem(
     badges: @Composable RowScope.() -> Unit = {},
     thumbnailContent: @Composable () -> Unit,
     trailingContent: @Composable RowScope.() -> Unit = {},
+    isActive: Boolean = false
 ) = ListItem(
     title = title,
     subtitle = {
@@ -164,7 +173,8 @@ fun ListItem(
     },
     thumbnailContent = thumbnailContent,
     trailingContent = trailingContent,
-    modifier = modifier
+    modifier = modifier,
+    isActive = isActive
 )
 
 @Composable
@@ -375,7 +385,8 @@ fun SongListItem(
         }
     },
     trailingContent = trailingContent,
-    modifier = modifier
+    modifier = modifier,
+    isActive = isActive
 )
 
 @Composable
@@ -1009,7 +1020,8 @@ fun MediaMetadataListItem(
         )
     },
     trailingContent = trailingContent,
-    modifier = modifier
+    modifier = modifier,
+    isActive = isActive
 )
 
 @Composable
@@ -1129,7 +1141,8 @@ fun YouTubeListItem(
         }
     },
     trailingContent = trailingContent,
-    modifier = modifier
+    modifier = modifier,
+    isActive = isActive
 )
 
 @Composable
