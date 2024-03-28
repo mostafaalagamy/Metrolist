e9f75f80645ca4314e0ebdd867dd0dc424146b13
Malopieds <malobeauchamps@proton.me>
2024-03-28 21:11:45 +0100

    fix(thumbnail): revert to upstream branch thumbnail file closes #20

--- app/src/main/java/com/zionhuang/music/ui/player/Thumbnail.kt
+++ app/src/main/java/com/zionhuang/music/ui/player/Thumbnail.kt
@@ -3,14 +3,17 @@ package com.zionhuang.music.ui.player
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,13 +25,9 @@ import com.zionhuang.music.LocalPlayerConnection
 import com.zionhuang.music.constants.PlayerHorizontalPadding
 import com.zionhuang.music.constants.ShowLyricsKey
 import com.zionhuang.music.constants.ThumbnailCornerRadius
-import com.zionhuang.music.extensions.metadata
 import com.zionhuang.music.ui.component.Lyrics
-import com.zionhuang.music.ui.utils.SnapLayoutInfoProvider
 import com.zionhuang.music.utils.rememberPreference
-import kotlinx.coroutines.flow.drop
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Thumbnail(
     sliderPositionProvider: () -> Long?,
@@ -37,39 +36,10 @@ fun Thumbnail(
     val playerConnection = LocalPlayerConnection.current ?: return
     val currentView = LocalView.current
 
-    val windows by playerConnection.queueWindows.collectAsState()
-    val currentWindowIndex by playerConnection.currentWindowIndex.collectAsState()
+    val mediaMetadata by playerConnection.mediaMetadata.collectAsState()
     val error by playerConnection.error.collectAsState()
 
-    val mediaMetadata by playerConnection.mediaMetadata.collectAsState()
-
     val showLyrics by rememberPreference(ShowLyricsKey, false)
-
-    val pagerState = rememberPagerState(
-        initialPage = currentWindowIndex.takeIf { it != -1 } ?: 0
-    )
-
-    val snapLayoutInfoProvider = remember(pagerState) {
-        SnapLayoutInfoProvider(
-            pagerState = pagerState,
-            positionInLayout = { _, _ -> 0f }
-        )
-    }
-
-    LaunchedEffect(pagerState, currentWindowIndex) {
-        try {
-            pagerState.scrollToPage(currentWindowIndex)
-        } catch (_: Exception) {
-        }
-    }
-
-    LaunchedEffect(pagerState) {
-        snapshotFlow { pagerState.settledPage }.drop(1).collect {
-            if (!pagerState.isScrollInProgress) {
-                playerConnection.player.seekToDefaultPosition(it)
-            }
-        }
-    }
 
     DisposableEffect(showLyrics) {
         currentView.keepScreenOn = showLyrics
@@ -87,48 +57,30 @@ fun Thumbnail(
                 .fillMaxSize()
                 .statusBarsPadding()
         ) {
-            windows.takeIf { it.isNotEmpty() }?.let { windows ->
-                HorizontalPager(
-                    state = pagerState,
-                    flingBehavior = rememberSnapFlingBehavior(snapLayoutInfoProvider),
-                    pageCount = windows.size,
-                    key = { windows[it].uid.hashCode() },
-                    beyondBoundsPageCount = 2,
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = PlayerHorizontalPadding)
+            ) {
+                AsyncImage(
+                    model = mediaMetadata?.thumbnailUrl,
+                    contentDescription = null,
                     modifier = Modifier
-                        .fillMaxSize()
-                        .statusBarsPadding()
-                ) { index ->
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(horizontal = PlayerHorizontalPadding)
-                    ) {
-                        val model = if (currentWindowIndex == index){
-                            mediaMetadata?.thumbnailUrl
-                        } else {
-                            windows[currentWindowIndex].mediaItem.metadata?.thumbnailUrl
-                        }
-                        AsyncImage(
-                            model = model,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(ThumbnailCornerRadius * 2))
-                                .pointerInput(Unit) {
-                                    detectTapGestures(
-                                        onDoubleTap = { offset ->
-                                            if (offset.x < size.width / 2) {
-                                                playerConnection.player.seekBack()
-                                            } else {
-                                                playerConnection.player.seekForward()
-                                            }
-                                        }
-                                    )
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(ThumbnailCornerRadius * 2))
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onDoubleTap = { offset ->
+                                    if (offset.x < size.width / 2) {
+                                        playerConnection.player.seekBack()
+                                    } else {
+                                        playerConnection.player.seekForward()
+                                    }
                                 }
-                        )
-                    }
-                }
+                            )
+                        }
+                )
             }
         }
 
@@ -137,9 +89,7 @@ fun Thumbnail(
             enter = fadeIn(),
             exit = fadeOut()
         ) {
-            Lyrics(
-                sliderPositionProvider = sliderPositionProvider
-            )
+            Lyrics(sliderPositionProvider = sliderPositionProvider)
         }
 
         AnimatedVisibility(
