ef4cb6e52064c291248e5c2afddb87fa9b4d2231
Malopieds <malobeauchamps@proton.me>
2024-07-06 18:29:44 +0200

    fix: add import

--- app/src/main/java/com/malopieds/innertune/ui/player/Player.kt
+++ app/src/main/java/com/malopieds/innertune/ui/player/Player.kt
@@ -53,6 +53,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
