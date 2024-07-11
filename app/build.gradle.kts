ec6c3eb5dc54568aec6d192b237ab471c988531d
Malopieds <malobeauchamps@proton.me>
2024-07-11 19:47:28 +0200

    bump: bump to v0.5.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.malopieds.innertune"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
-        versionName = "0.5.7"
+        versionCode = 25
+        versionName = "0.5.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
