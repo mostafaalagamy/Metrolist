78bd0f756bcf2d316364d1e0f7d719736a20d226
moxxaxx <darshalagamy@gmail.com>
2024-07-17 11:40:28 +0300

    Add files via upload

--- app/src/main/java/com/malopieds/innertune/ui/player/Queue.kt
+++ app/src/main/java/com/malopieds/innertune/ui/player/Queue.kt
@@ -3,6 +3,14 @@ package com.malopieds.innertune.ui.player
 import android.annotation.SuppressLint
 import android.text.format.Formatter
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,6 +30,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -84,6 +93,7 @@ import com.malopieds.innertune.ui.component.BottomSheet
 import com.malopieds.innertune.ui.component.BottomSheetState
 import com.malopieds.innertune.ui.component.LocalMenuState
 import com.malopieds.innertune.ui.component.MediaMetadataListItem
+import com.malopieds.innertune.ui.component.ResizableIconButton
 import com.malopieds.innertune.ui.menu.PlayerMenu
 import com.malopieds.innertune.ui.menu.SelectionMediaMetadataMenu
 import com.malopieds.innertune.utils.makeTimeString
@@ -118,6 +128,9 @@ fun Queue(
 
     val selectedSongs: MutableList<MediaMetadata> = mutableStateListOf()
     val selectedItems: MutableList<Timeline.Window> = mutableStateListOf()
+    var selection by remember {
+        mutableStateOf(false)
+    }
 
     var showDetailsDialog by rememberSaveable {
         mutableStateOf(false)
@@ -267,8 +280,8 @@ fun Queue(
                 WindowInsets.systemBars
                     .add(
                         WindowInsets(
-                            top = ListItemHeight + 12.dp + 8.dp,
-                            bottom = ListItemHeight + 8.dp,
+                            top = ListItemHeight,
+                            bottom = ListItemHeight,
                         ),
                     ).asPaddingValues(),
             modifier =
@@ -276,6 +289,15 @@ fun Queue(
                     .reorderable(reorderableState)
                     .nestedScroll(state.preUpPostDownNestedScrollConnection),
         ) {
+            item {
+                Spacer(
+                    modifier =
+                        Modifier
+                            .animateContentSize()
+                            .height(if (selection) 64.dp else 0.dp),
+                )
+            }
+
             itemsIndexed(
                 items = mutableQueueWindows,
                 key = { _, item -> item.uid.hashCode() },
@@ -306,40 +328,9 @@ fun Queue(
 //                        state = dismissState,
 //                        = {
                     ) {
-                        Row(
-                            horizontalArrangement = Arrangement.Center,
-                        ) {
-                            IconButton(
-                                modifier =
-                                    Modifier
-                                        .align(Alignment.CenterVertically),
-                                onClick = {
-                                    if (window.mediaItem.metadata!! in selectedSongs) {
-                                        selectedSongs.remove(window.mediaItem.metadata!!)
-                                        selectedItems.remove(currentItem)
-                                    } else {
-                                        selectedSongs.add(window.mediaItem.metadata!!)
-                                        selectedItems.add(currentItem)
-                                    }
-                                },
-                            ) {
-                                Icon(
-                                    painter =
-                                        painterResource(
-                                            if (window.mediaItem.metadata!! in
-                                                selectedSongs
-                                            ) {
-                                                R.drawable.check_box
-                                            } else {
-                                                R.drawable.uncheck_box
-                                            },
-                                        ),
-                                    contentDescription = null,
-                                    tint = LocalContentColor.current,
-                                )
-                            }
                             MediaMetadataListItem(
                                 mediaMetadata = window.mediaItem.metadata!!,
+                                isSelected = selection && window.mediaItem.metadata!! in selectedSongs,
                                 isActive = index == currentWindowIndex,
                                 isPlaying = isPlaying,
                                 trailingContent = {
@@ -360,7 +351,15 @@ fun Queue(
                                         .fillMaxWidth()
                                         .combinedClickable(
                                             onClick = {
-                                                if (selectedSongs.isEmpty()) {
+                                                if (selection) {
+                                                    if (window.mediaItem.metadata!! in selectedSongs) {
+                                                        selectedSongs.remove(window.mediaItem.metadata!!)
+                                                        selectedItems.remove(currentItem)
+                                                    } else {
+                                                        selectedSongs.add(window.mediaItem.metadata!!)
+                                                        selectedItems.add(currentItem)
+                                                    }
+                                                } else {
                                                     if (index == currentWindowIndex) {
                                                         playerConnection.player.togglePlayPause()
                                                     } else {
@@ -368,14 +367,6 @@ fun Queue(
                                                             window.firstPeriodIndex,
                                                         )
                                                         playerConnection.player.playWhenReady = true
-                                                    }
-                                                } else {
-                                                    if (window.mediaItem.metadata!! in selectedSongs) {
-                                                        selectedSongs.remove(window.mediaItem.metadata!!)
-                                                        selectedItems.remove(currentItem)
-                                                    } else {
-                                                        selectedSongs.add(window.mediaItem.metadata!!)
-                                                        selectedItems.add(currentItem)
                                                     }
                                                 }
                                             },
@@ -396,13 +387,12 @@ fun Queue(
                                         ),
                                 // .detectReorderAfterLongPress(reorderableState)
                             )
-                        }
                     }
                 }
             }
         }
 
-        Box(
+        Column(
             modifier =
                 Modifier
                     .background(
@@ -419,8 +409,7 @@ fun Queue(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier =
                     Modifier
-                        .height(ListItemHeight + 12.dp)
-                        .padding(horizontal = 12.dp),
+                        .padding(horizontal = 12.dp, vertical = 12.dp),
             ) {
                 Text(
                     text = queueTitle.orEmpty(),
@@ -430,19 +419,80 @@ fun Queue(
                     modifier = Modifier.weight(1f),
                 )
 
-                if (selectedSongs.isNotEmpty()) {
+                AnimatedVisibility(
+                    visible = !selection,
+                    enter = fadeIn() + slideInVertically { it },
+                    exit = fadeOut() + slideOutVertically { it },
+                ) {
                     IconButton(
                         onClick = {
-                            selectedSongs.clear()
-                            selectedItems.clear()
+                            selection = true
                         },
                     ) {
                         Icon(
-                            painter = painterResource(R.drawable.deselect),
+                            painter = painterResource(R.drawable.select_all),
                             contentDescription = null,
-                            tint = LocalContentColor.current,
                         )
                     }
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                    horizontalAlignment = Alignment.End,
+                ) {
+                    Text(
+                        text = pluralStringResource(R.plurals.n_song, queueWindows.size, queueWindows.size),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+
+                    Text(
+                        text = makeTimeString(queueLength * 1000L),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+
+            AnimatedVisibility(
+                visible = selection,
+                enter = fadeIn() + expandVertically(),
+                exit = fadeOut() + shrinkVertically(),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .height(64.dp)
+                        .padding(start = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    val count = selectedSongs.size
+                    Text(text = "$count elements selected", modifier = Modifier.weight(1f))
+                    IconButton(
+                        onClick = {
+                            if (count == mutableQueueWindows.size) {
+                                selectedSongs.clear()
+                                selectedItems.clear()
+                            } else {
+                                queueWindows
+                                    .filter { it.mediaItem.metadata!! !in selectedSongs }
+                                    .forEach {
+                                        selectedSongs.add(it.mediaItem.metadata!!)
+                                        selectedItems.add(it)
+                                    }
+                            }
+                        },
+                    ) {
+                        Icon(
+                            painter =
+                            painterResource(
+                                if (count == mutableQueueWindows.size) {
+                                    R.drawable.deselect
+                                } else {
+                                    R.drawable.select_all
+                                },
+                            ),
+                            contentDescription = null,
+                        )
+                    }
+
                     IconButton(
                         onClick = {
                             menuState.show {
@@ -464,36 +514,15 @@ fun Queue(
                             tint = LocalContentColor.current,
                         )
                     }
-                } else {
+
                     IconButton(
-                        onClick = {
-                            queueWindows.forEach {
-                                selectedSongs.add(it.mediaItem.metadata!!)
-                                selectedItems.add(it)
-                            }
-                        },
+                        onClick = { selection = false },
                     ) {
                         Icon(
-                            painter = painterResource(R.drawable.select_all),
+                            painter = painterResource(R.drawable.close),
                             contentDescription = null,
-                            tint = LocalContentColor.current,
                         )
                     }
-                }
-
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(4.dp),
-                    horizontalAlignment = Alignment.End,
-                ) {
-                    Text(
-                        text = pluralStringResource(R.plurals.n_song, queueWindows.size, queueWindows.size),
-                        style = MaterialTheme.typography.bodyMedium,
-                    )
-
-                    Text(
-                        text = makeTimeString(queueLength * 1000L),
-                        style = MaterialTheme.typography.bodyMedium,
-                    )
                 }
             }
         }
@@ -544,22 +573,21 @@ fun Queue(
                 modifier = Modifier.align(Alignment.Center),
             )
 
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterEnd),
+            ResizableIconButton(
+                icon =
+                    when (repeatMode) {
+                        Player.REPEAT_MODE_OFF, Player.REPEAT_MODE_ALL -> R.drawable.repeat
+                        Player.REPEAT_MODE_ONE -> R.drawable.repeat_one
+                        else -> throw IllegalStateException()
+                    },
+                modifier =
+                    Modifier
+                        .size(32.dp)
+                        .padding(4.dp)
+                        .align(Alignment.CenterEnd)
+                        .alpha(if (repeatMode == Player.REPEAT_MODE_OFF) 0.5f else 1f),
                 onClick = playerConnection.player::toggleRepeatMode,
-            ) {
-                Icon(
-                    painter = painterResource(
-                        when (repeatMode) {
-                            Player.REPEAT_MODE_OFF, Player.REPEAT_MODE_ALL -> R.drawable.repeat
-                            Player.REPEAT_MODE_ONE -> R.drawable.repeat_one
-                            else -> throw IllegalStateException()
-                        }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.alpha(if (repeatMode == Player.REPEAT_MODE_OFF) 0.5f else 1f),
-                )
-            }
+            )
         }
     }
 }
