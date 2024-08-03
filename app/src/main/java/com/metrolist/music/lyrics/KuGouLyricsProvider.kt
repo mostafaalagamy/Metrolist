effc8b4d1ece3ff6ce2d88a3d0d54e23ffdf72f5
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-03 07:16:16 +0300

    kugou: Improve code

--- app/src/main/java/com/metrolist/music/lyrics/KuGouLyricsProvider.kt
+++ app/src/main/java/com/metrolist/music/lyrics/KuGouLyricsProvider.kt
@@ -8,23 +8,13 @@ import com.metrolist.kugou.KuGou
 
 object KuGouLyricsProvider : LyricsProvider {
     override val name = "Kugou"
+    override fun isEnabled(context: Context): Boolean =
+        context.dataStore[EnableKugouKey] ?: true
 
-    override fun isEnabled(context: Context): Boolean = context.dataStore[EnableKugouKey] ?: true
+    override suspend fun getLyrics(id: String, title: String, artist: String, duration: Int): Result<String> =
+        KuGou.getLyrics(title, artist, duration)
 
-    override suspend fun getLyrics(
-        id: String,
-        title: String,
-        artist: String,
-        duration: Int,
-    ): Result<String> = KuGou.getLyrics(title, artist, duration)
-
-    override suspend fun getAllLyrics(
-        id: String,
-        title: String,
-        artist: String,
-        duration: Int,
-        callback: (String) -> Unit,
-    ) {
-        KuGou.getAllLyrics(title, artist, duration, callback)
+    override suspend fun getAllLyrics(id: String, title: String, artist: String, duration: Int, callback: (String) -> Unit) {
+        KuGou.getAllPossibleLyricsOptions(title, artist, duration, callback)
     }
 }
