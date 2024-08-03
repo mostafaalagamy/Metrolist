f2e2ca6717dcd24c332b5d390ac7b1e641144980
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-03 06:59:30 +0300

    kugou: Improve code and delete cc4j

--- kugou/src/main/java/com/metrolist/kugou/KuGou.kt
+++ kugou/src/main/java/com/metrolist/kugou/KuGou.kt
@@ -1,15 +1,13 @@
 package com.metrolist.kugou
 
 import com.metrolist.kugou.models.DownloadLyricsResponse
+import com.metrolist.kugou.models.Keyword
 import com.metrolist.kugou.models.SearchLyricsResponse
 import com.metrolist.kugou.models.SearchSongResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.BrowserUserAgent
 import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.http.ContentType
@@ -18,230 +16,164 @@ import io.ktor.serialization.kotlinx.json.json
 import io.ktor.util.decodeBase64String
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import java.lang.Integer.min
 import kotlin.math.abs
+
+@OptIn(ExperimentalSerializationApi::class)
+private val client = HttpClient {
+    expectSuccess = true
+
+    install(ContentNegotiation) {
+        val json = Json {
+            ignoreUnknownKeys = true
+            explicitNulls = false
+            encodeDefaults = true
+        }
+        json(json)
+        json(json, ContentType.Text.Html)
+        json(json, ContentType.Text.Plain)
+    }
+
+    install(ContentEncoding) {
+        gzip()
+        deflate()
+    }
+}
+
+private const val PAGE_SIZE = 8
+private const val HEAD_CUT_LIMIT = 30
 
 /**
  * KuGou Lyrics Library
  * Modified from [ViMusic](https://github.com/vfsfitvnm/ViMusic)
- * Modified from [ViTune](https://github.com/25huizengek1/ViTune)
  */
-
 object KuGou {
-    @OptIn(ExperimentalSerializationApi::class)
-    private val client by lazy {
-        HttpClient(OkHttp) {
-            BrowserUserAgent()
+    var useTraditionalChinese: Boolean = false
 
-            expectSuccess = true
-
-            install(ContentNegotiation) {
-                val feature =
-                    Json {
-                        ignoreUnknownKeys = true
-                        explicitNulls = false
-                        encodeDefaults = true
-                    }
-
-                json(feature)
-                json(feature, ContentType.Text.Html)
-                json(feature, ContentType.Text.Plain)
-            }
-
-            install(ContentEncoding) {
-                gzip()
-                deflate()
-            }
-
-            defaultRequest {
-                url("https://krcs.kugou.com")
-            }
-        }
-    }
-
-    suspend fun getLyrics(
-        artist: String,
-        title: String,
-        duration: Int,
-    ) = runCatching {
-        val keyword = keyword(artist, title)
-        val infoByKeyword = searchSong(keyword)
-
-        if (infoByKeyword.isNotEmpty()) {
-            var tolerance = 0
-
-            while (tolerance <= 5) {
-                for (info in infoByKeyword) {
-                    if (info.duration >= duration - tolerance && info.duration <= duration + tolerance) {
-                        searchLyricsByHash(info.hash).firstOrNull()?.let { candidate ->
-                            return@runCatching downloadLyrics(
-                                candidate.id,
-                                candidate.accesskey,
-                            ).normalize().value
-                        }
-                    }
-                }
-
-                tolerance++
-            }
+    suspend fun getLyrics(title: String, artist: String, duration: Int): Result<String> =
+        runCatching {
+            val keyword = generateKeyword(title, artist)
+            getLyricsCandidate(keyword, duration)?.let { candidate ->
+                downloadLyrics(candidate.id, candidate.accesskey).content.decodeBase64String()
+                    .normalize()
+            } ?: throw IllegalStateException("No lyrics candidate")
         }
 
-        searchLyricsByKeyword(keyword).firstOrNull()?.let { candidate ->
-            return@runCatching downloadLyrics(
-                candidate.id,
-                candidate.accesskey,
-            ).normalize().value
-        }
-
-        throw IllegalStateException("Lyrics endpoint not found")
-    }
-
-    suspend fun getAllLyrics(
-        title: String,
-        artist: String,
-        duration: Int,
-        callback: (String) -> Unit,
+    suspend fun getAllPossibleLyricsOptions(
+        title: String, artist: String, duration: Int, callback: (String) -> Unit
     ) {
-        val keyword = keyword(title, artist)
-        searchSong(keyword).forEach {
+        val keyword = generateKeyword(title, artist)
+        searchSongs(keyword).data.info.forEach {
             if (duration == -1 || abs(it.duration - duration) <= DURATION_TOLERANCE) {
-                searchLyricsByHash(it.hash).firstOrNull()?.let { candidate ->
-                    downloadLyrics(candidate.id, candidate.accesskey).normalize().value.let(callback)
+                searchLyricsByHash(it.hash).candidates.firstOrNull()?.let { candidate ->
+                    downloadLyrics(candidate.id, candidate.accesskey).content.decodeBase64String()
+                        .normalize().let(callback)
                 }
             }
         }
-        searchLyricsByKeyword(keyword).forEach { candidate ->
-            downloadLyrics(candidate.id, candidate.accesskey)
-                .normalize()
-                .value
-                .let(callback)
+        searchLyricsByKeyword(keyword, duration).candidates.forEach { candidate ->
+            downloadLyrics(candidate.id, candidate.accesskey).content.decodeBase64String()
+                .normalize().let(callback)
         }
     }
 
-    private suspend fun downloadLyrics(
-        id: Long,
-        accessKey: String,
-    ) = client
-        .get("/download") {
+    suspend fun getLyricsCandidate(
+        keyword: Keyword, duration: Int
+    ): SearchLyricsResponse.Candidate? {
+        searchSongs(keyword).data.info.forEach { song ->
+            if (duration == -1 || abs(song.duration - duration) <= DURATION_TOLERANCE) { // if duration == -1, we don't care duration
+                val candidate = searchLyricsByHash(song.hash).candidates.firstOrNull()
+                if (candidate != null) return candidate
+            }
+        }
+        return searchLyricsByKeyword(keyword, duration).candidates.firstOrNull()
+    }
+
+    suspend fun searchSongs(keyword: Keyword) =
+        client.get("https://mobileservice.kugou.com/api/v3/search/song") {
+            parameter("version", 9108)
+            parameter("plat", 0)
+            parameter("pagesize", PAGE_SIZE)
+            parameter("showtype", 0)
+            url.encodedParameters.append(
+                "keyword",
+                "${keyword.title} - ${keyword.artist}".encodeURLParameter(spaceToPlus = false)
+            )
+        }.body<SearchSongResponse>()
+
+    private suspend fun searchLyricsByKeyword(keyword: Keyword, duration: Int) =
+        client.get("https://lyrics.kugou.com/search") {
             parameter("ver", 1)
             parameter("man", "yes")
             parameter("client", "pc")
+            parameter(
+                "duration", duration.takeIf { it != -1 }?.times(1000)
+            ) // if duration == -1, we don't care duration
+            url.encodedParameters.append(
+                "keyword",
+                "${keyword.title} - ${keyword.artist}".encodeURLParameter(spaceToPlus = false)
+            )
+        }.body<SearchLyricsResponse>()
+
+    private suspend fun searchLyricsByHash(hash: String) =
+        client.get("https://lyrics.kugou.com/search") {
+            parameter("ver", 1)
+            parameter("man", "yes")
+            parameter("client", "pc")
+            parameter("hash", hash)
+        }.body<SearchLyricsResponse>()
+
+    private suspend fun downloadLyrics(id: Long, accessKey: String) =
+        client.get("https://lyrics.kugou.com/download") {
             parameter("fmt", "lrc")
+            parameter("charset", "utf8")
+            parameter("client", "pc")
+            parameter("ver", 1)
             parameter("id", id)
             parameter("accesskey", accessKey)
         }.body<DownloadLyricsResponse>()
-        .content
-        .decodeBase64String()
-        .let(KuGou::Lyrics)
 
-    private suspend fun searchLyricsByHash(hash: String) =
-        client
-            .get("/search") {
-                parameter("ver", 1)
-                parameter("man", "yes")
-                parameter("client", "mobi")
-                parameter("hash", hash)
-            }.body<SearchLyricsResponse>()
-            .candidates
+    private fun normalizeTitle(title: String) =
+        title.replace("\\(.*\\)".toRegex(), "").replace("（.*）".toRegex(), "")
+            .replace("「.*」".toRegex(), "").replace("『.*』".toRegex(), "")
+            .replace("<.*>".toRegex(), "").replace("《.*》".toRegex(), "")
+            .replace("〈.*〉".toRegex(), "").replace("＜.*＞".toRegex(), "")
 
-    private suspend fun searchLyricsByKeyword(keyword: String) =
-        client
-            .get("/search") {
-                parameter("ver", 1)
-                parameter("man", "yes")
-                parameter("client", "mobi")
-                url.encodedParameters.append("keyword", keyword.encodeURLParameter(spaceToPlus = false))
-            }.body<SearchLyricsResponse>()
-            .candidates
+    private fun normalizeArtist(artist: String) =
+        artist.replace(", ", "、").replace(" & ", "、").replace(".", "").replace("和", "、")
+            .replace("\\(.*\\)".toRegex(), "").replace("（.*）".toRegex(), "")
 
-    private suspend fun searchSong(keyword: String) =
-        client
-            .get("https://mobileservice.kugou.com/api/v3/search/song") {
-                parameter("version", 9108)
-                parameter("plat", 0)
-                parameter("pagesize", 8)
-                parameter("showtype", 0)
-                url.encodedParameters.append("keyword", keyword.encodeURLParameter(spaceToPlus = false))
-            }.body<SearchSongResponse>()
-            .data.info
+    fun generateKeyword(title: String, artist: String) =
+        Keyword(normalizeTitle(title), normalizeArtist(artist))
 
-    private fun keyword(
-        artist: String,
-        title: String,
-    ): String {
-        val (newTitle, featuring) = title.extract(" (feat. ", ')')
-
-        val newArtist =
-            (if (featuring.isEmpty()) artist else "$artist, $featuring")
-                .replace(", ", "、")
-                .replace(" & ", "、")
-                .replace(".", "")
-
-        return "$newArtist - $newTitle"
-    }
-
-    @Suppress("ReturnCount")
-    private fun String.extract(
-        startDelimiter: String,
-        endDelimiter: Char,
-    ): Pair<String, String> {
-        val startIndex = indexOf(startDelimiter).takeIf { it != -1 } ?: return this to ""
-        val endIndex = indexOf(endDelimiter, startIndex).takeIf { it != -1 } ?: return this to ""
-
-        return removeRange(startIndex, endIndex + 1) to substring(startIndex + startDelimiter.length, endIndex)
-    }
-
-    @JvmInline
-    value class Lyrics(
-        val value: String,
-    ) {
-        @Suppress("CyclomaticComplexMethod")
-        fun normalize(): Lyrics {
-            var toDrop = 0
-            var maybeToDrop = 0
-
-            val text = value.replace("\r\n", "\n").trim()
-
-            for (line in text.lineSequence()) {
-                when {
-                    line.startsWith("[ti:") ||
-                        line.startsWith("[ar:") ||
-                        line.startsWith("[al:") ||
-                        line.startsWith("[by:") ||
-                        line.startsWith("[hash:") ||
-                        line.startsWith("[sign:") ||
-                        line.startsWith("[qq:") ||
-                        line.startsWith("[total:") ||
-                        line.startsWith("[offset:") ||
-                        line.startsWith("[id:") ||
-                        line.containsAt("]Written by：", 9) ||
-                        line.containsAt("]Lyrics by：", 9) ||
-                        line.containsAt("]Composed by：", 9) ||
-                        line.containsAt("]Producer：", 9) ||
-                        line.containsAt("]作曲 : ", 9) ||
-                        line.containsAt("]作词 : ", 9) -> {
-                        toDrop += line.length + 1 + maybeToDrop
-                        maybeToDrop = 0
-                    }
-
-                    maybeToDrop == 0 -> maybeToDrop = line.length + 1
-
-                    else -> {
-                        maybeToDrop = 0
+    private fun String.normalize(): String =
+        replace("&apos;", "'").lines().filter { line -> line.matches(ACCEPTED_REGEX) }
+            .let { lines ->
+                // Remove useless information such as singer, writer, composer, guitar, etc.
+                var headCutLine = 0
+                for (i in min(HEAD_CUT_LIMIT, lines.lastIndex) downTo 0) {
+                    if (lines[i].matches(BANNED_REGEX)) {
+                        headCutLine = i + 1
                         break
                     }
                 }
+                val filteredLines = lines.drop(headCutLine)
+
+                var tailCutLine = 0
+                for (i in min(lines.size - HEAD_CUT_LIMIT, lines.lastIndex) downTo 0) {
+                    if (lines[lines.lastIndex - i].matches(BANNED_REGEX)) {
+                        tailCutLine = i + 1
+                        break
+                    }
+                }
+                val finalLines = filteredLines.dropLast(tailCutLine)
+
+                return@let finalLines.joinToString("\n")
             }
 
-            return Lyrics(text.drop(toDrop + maybeToDrop).removeHtmlEntities())
-        }
-
-        private fun String.containsAt(
-            charSequence: CharSequence,
-            startIndex: Int,
-        ) = regionMatches(startIndex, charSequence, 0, charSequence.length)
-
-        private fun String.removeHtmlEntities() = replace("&apos;", "'")
-    }
+    @Suppress("RegExpRedundantEscape")
+    private val ACCEPTED_REGEX = "\\[(\\d\\d):(\\d\\d)\\.(\\d{2,3})\\].*".toRegex()
+    private val BANNED_REGEX = ".+].+[:：].+".toRegex()
 
     private const val DURATION_TOLERANCE = 8
 }
