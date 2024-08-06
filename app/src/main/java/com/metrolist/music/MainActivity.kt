781a9d588ffe6cb3a78460857532bd63a17f04af
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-07 00:10:24 +0300

    Fix actions for shortcuts

--- app/src/main/java/com/metrolist/music/MainActivity.kt
+++ app/src/main/java/com/metrolist/music/MainActivity.kt
@@ -931,7 +931,6 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         const val ACTION_SEARCH = "com.metrolist.music.action.SEARCH"
-        const val ACTION_EXPLORE = "com.metrolist.music.action.EXPLORE"
         const val ACTION_LIBRARY = "com.metrolist.music.action.LIBRARY"
     }
 }
