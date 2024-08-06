22f4715e5f33989059204285159d44da92853315
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-07 00:21:24 +0300

    Update: fix actions for shortcuts

--- app/src/main/java/com/metrolist/music/MainActivity.kt
+++ app/src/main/java/com/metrolist/music/MainActivity.kt
@@ -227,6 +227,7 @@ class MainActivity : ComponentActivity() {
                         remember {
                             when (intent?.action) {
                                 ACTION_LIBRARY -> NavigationTab.LIBRARY
+                                ACTION_EXPLORE -> NavigationTab.EXPLORE
                                 else -> null
                             }
                         }
@@ -931,6 +932,7 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         const val ACTION_SEARCH = "com.metrolist.music.action.SEARCH"
+        const val ACTION_EXPLORE = "com.metrolist.music.action.EXPLORE"
         const val ACTION_LIBRARY = "com.metrolist.music.action.LIBRARY"
     }
 }
