8a5615609a72251d8ee2ec2df66ea27420dfac33
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-07 04:25:18 +0300

    Update: improve Top Playlist Screen

--- app/src/main/java/com/metrolist/music/ui/screens/playlist/TopPlaylistScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/playlist/TopPlaylistScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
