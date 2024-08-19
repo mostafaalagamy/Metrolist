droidudc/InnerTune

feat: player text alignment customization

app/src/main/java/com/malopieds/innertune/ui/screens/settings/AppearanceSettings.kt
@@ -26,6 +26,7 @@ import com.malopieds.innertune.constants.LyricsClickKey
 import com.malopieds.innertune.constants.LyricsTextPositionKey
 import com.malopieds.innertune.constants.PlayerBackgroundStyle
 import com.malopieds.innertune.constants.PlayerBackgroundStyleKey
+import com.malopieds.innertune.constants.PlayerTextAlignmentKey
 import com.malopieds.innertune.constants.PureBlackKey
 import com.malopieds.innertune.constants.SwipeThumbnailKey
 import com.malopieds.innertune.ui.component.EnumListPreference
@@ -51,6 +52,11 @@ fun AppearanceSettings(
     val (pureBlack, onPureBlackChange) = rememberPreference(PureBlackKey, defaultValue = false)
     val (defaultOpenTab, onDefaultOpenTabChange) = rememberEnumPreference(DefaultOpenTabKey, defaultValue = NavigationTab.HOME)
     val (lyricsPosition, onLyricsPositionChange) = rememberEnumPreference(LyricsTextPositionKey, defaultValue = LyricsPosition.CENTER)
+    val (playerTextAlignment, onPlayerTextAlignmentChange) =
+        rememberEnumPreference(
+            PlayerTextAlignmentKey,
+            defaultValue = PlayerTextAlignment.CENTER,
+        )
     val (lyricsClick, onLyricsClickChange) = rememberPreference(LyricsClickKey, defaultValue = true)
     val (squigglySlider, onSquigglySliderChange) = rememberPreference(EnableSquigglySlider, defaultValue = true)
     val (swipeThumbnail, onSwipeThumbnailChange) = rememberPreference(SwipeThumbnailKey, defaultValue = true)
@@ -128,6 +134,18 @@ fun AppearanceSettings(
             },
         )
         EnumListPreference(
+            title = { Text(stringResource(R.string.player_text_alignment)) },
+            icon = { Icon(painterResource(R.drawable.format_align_center), null) },
+            selectedValue = playerTextAlignment,
+            onValueSelected = onPlayerTextAlignmentChange,
+            valueText = {
+                when (it) {
+                    PlayerTextAlignment.SIDED -> stringResource(R.string.sided)
+                    PlayerTextAlignment.CENTER -> stringResource(R.string.center)
+                }
+            },
+        )
+        EnumListPreference(
             title = { Text(stringResource(R.string.lyrics_text_position)) },
             icon = { Icon(painterResource(R.drawable.lyrics), null) },
             selectedValue = lyricsPosition,
@@ -193,4 +211,9 @@ enum class LyricsPosition {
     LEFT,
     CENTER,
     RIGHT,
+}
+
+enum class PlayerTextAlignment {
+    SIDED,
+    CENTER,
 }

mostafaalagamy/Metrolist

app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
 import com.metrolist.music.LocalPlayerAwareWindowInsets
 import com.metrolist.music.R
 import com.metrolist.music.constants.DarkModeKey
@@ -37,6 +38,29 @@ import com.metrolist.music.ui.component.SwitchPreference
 import com.metrolist.music.ui.utils.backToMain
 import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
+=======
+import com.metrolist.music.LocalPlayerAwareWindowInsets
+import com.metrolist.music.R
+import com.metrolist.music.constants.DarkModeKey
+import com.metrolist.music.constants.DefaultOpenTabKey
+import com.metrolist.music.constants.DynamicThemeKey
+import com.metrolist.music.constants.EnableSquigglySlider
+import com.metrolist.music.constants.GridItemSize
+import com.metrolist.music.constants.GridItemsSizeKey
+import com.metrolist.music.constants.LyricsClickKey
+import com.metrolist.music.constants.LyricsTextPositionKey
+import com.metrolist.music.constants.PlayerBackgroundStyle
+import com.metrolist.music.constants.PlayerBackgroundStyleKey
+import com.metrolist.music.constants.PlayerTextAlignmentKey
+import com.metrolist.music.constants.PureBlackKey
+import com.metrolist.music.constants.SwipeThumbnailKey
+import com.metrolist.music.ui.component.EnumListPreference
+import com.metrolist.music.ui.component.IconButton
+import com.metrolist.music.ui.component.SwitchPreference
+import com.metrolist.music.ui.utils.backToMain
+import com.metrolist.music.utils.rememberEnumPreference
+import com.metrolist.music.utils.rememberPreference
+>>>>>>> e8d0e4e0 (feat: player text alignment customization):app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,6 +78,11 @@ fun AppearanceSettings(
     val (pureBlack, onPureBlackChange) = rememberPreference(PureBlackKey, defaultValue = false)
     val (defaultOpenTab, onDefaultOpenTabChange) = rememberEnumPreference(DefaultOpenTabKey, defaultValue = NavigationTab.HOME)
     val (lyricsPosition, onLyricsPositionChange) = rememberEnumPreference(LyricsTextPositionKey, defaultValue = LyricsPosition.CENTER)
+    val (playerTextAlignment, onPlayerTextAlignmentChange) =
+        rememberEnumPreference(
+            PlayerTextAlignmentKey,
+            defaultValue = PlayerTextAlignment.CENTER,
+        )
     val (lyricsClick, onLyricsClickChange) = rememberPreference(LyricsClickKey, defaultValue = true)
     val (squigglySlider, onSquigglySliderChange) = rememberPreference(EnableSquigglySlider, defaultValue = true)
     val (swipeThumbnail, onSwipeThumbnailChange) = rememberPreference(SwipeThumbnailKey, defaultValue = true)
@@ -140,9 +169,14 @@ fun AppearanceSettings(
         )
         EnumListPreference(
 <<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
 =======
             title = { Text(stringResource(R.string.player_text_alignment)) },
             icon = { Icon(painterResource(R.drawable.format_align_left), null) },
+=======
+            title = { Text(stringResource(R.string.player_text_alignment)) },
+            icon = { Icon(painterResource(R.drawable.format_align_center), null) },
+>>>>>>> e8d0e4e0 (feat: player text alignment customization):app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
             selectedValue = playerTextAlignment,
             onValueSelected = onPlayerTextAlignmentChange,
             valueText = {
@@ -153,7 +187,10 @@ fun AppearanceSettings(
             },
         )
         EnumListPreference(
->>>>>>> 4279d383 (feat: update icons and add animations in settings):app/src/main/java/com/malopieds/innertune/ui/screens/settings/AppearanceSettings.kt
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
+>>>>>>> 4279d383 (feat: update icons and add animations in settings):app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
+=======
+>>>>>>> e8d0e4e0 (feat: player text alignment customization):app/src/main/java/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
             title = { Text(stringResource(R.string.lyrics_text_position)) },
             icon = { Icon(painterResource(R.drawable.lyrics), null) },
             selectedValue = lyricsPosition,
@@ -219,4 +256,9 @@ enum class LyricsPosition {
     LEFT,
     CENTER,
     RIGHT,
+}
+
+enum class PlayerTextAlignment {
+    SIDED,
+    CENTER,
 }