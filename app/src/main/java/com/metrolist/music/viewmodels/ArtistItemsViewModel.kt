0114299115ce92c74fc180e359b2ce3f4862073b
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-19 03:31:45 +0300

    Update ArtistItemsViewModel

--- app/src/main/java/com/metrolist/music/viewmodels/ArtistItemsViewModel.kt
+++ app/src/main/java/com/metrolist/music/viewmodels/ArtistItemsViewModel.kt
@@ -4,12 +4,6 @@ import android.content.Context
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-<<<<<<< HEAD:app/src/main/java/com/metrolist/music/viewmodels/ArtistItemsViewModel.kt
-import com.metrolist.innertube.YouTube
-import com.metrolist.innertube.models.BrowseEndpoint
-import com.metrolist.music.models.ItemsPage
-import com.metrolist.music.utils.reportException
-=======
 import com.metrolist.innertube.YouTube
 import com.metrolist.innertube.models.BrowseEndpoint
 import com.metrolist.innertube.models.filterExplicit
@@ -18,7 +12,6 @@ import com.metrolist.music.models.ItemsPage
 import com.metrolist.music.utils.dataStore
 import com.metrolist.music.utils.get
 import com.metrolist.music.utils.reportException
->>>>>>> a3851bbf (feat: option to hide explicit content):app/src/main/java/com/metrolist/music/viewmodels/ArtistItemsViewModel.kt
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
