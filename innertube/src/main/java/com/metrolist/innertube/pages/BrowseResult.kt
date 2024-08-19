2c767b7a90f3bf5c368ffd02466e706e705f9226
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-08-19 03:42:12 +0300

    Update BrowseResult

--- innertube/src/main/java/com/metrolist/innertube/pages/BrowseResult.kt
+++ innertube/src/main/java/com/metrolist/innertube/pages/BrowseResult.kt
@@ -1,11 +1,7 @@
 package com.metrolist.innertube.pages
 
-<<<<<<< HEAD:innertube/src/main/java/com/metrolist/innertube/pages/BrowseResult.kt
-import com.metrolist.innertube.models.YTItem
-=======
 import com.metrolist.innertube.models.YTItem
 import com.metrolist.innertube.models.filterExplicit
->>>>>>> a3851bbf (feat: option to hide explicit content):innertube/src/main/java/com/metrolist/innertube/pages/BrowseResult.kt
 
 data class BrowseResult(
     val title: String?,
