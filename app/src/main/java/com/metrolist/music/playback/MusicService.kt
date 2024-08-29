Maloy-Android/Muzza

feat: auto skip next on error

app/src/main/java/com/zionhuang/music/playback/MusicService.kt
@@ -59,6 +59,7 @@ import com.zionhuang.music.R
 import com.zionhuang.music.constants.AudioNormalizationKey
 import com.zionhuang.music.constants.AudioQuality
 import com.zionhuang.music.constants.AudioQualityKey
+import com.zionhuang.music.constants.AutoSkipNextOnErrorKey
 import com.zionhuang.music.constants.DiscordTokenKey
 import com.zionhuang.music.constants.EnableDiscordRPCKey
 import com.zionhuang.music.constants.HideExplicitKey
@@ -580,6 +581,14 @@ class MusicService : MediaLibraryService(),
             dataStore.edit { settings ->
                 settings[RepeatModeKey] = repeatMode
             }
+        }
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        if (dataStore.get(AutoSkipNextOnErrorKey, false) && player.hasNextMediaItem()) {
+            player.seekToNext()
+            player.prepare()
+            player.playWhenReady = true
         }
     }
 

app/src/main/java/com/zionhuang/music/ui/screens/settings/PlayerSettings.kt
@@ -22,6 +22,7 @@ import com.zionhuang.music.R
 import com.zionhuang.music.constants.AudioNormalizationKey
 import com.zionhuang.music.constants.AudioQuality
 import com.zionhuang.music.constants.AudioQualityKey
+import com.zionhuang.music.constants.AutoSkipNextOnErrorKey
 import com.zionhuang.music.constants.PersistentQueueKey
 import com.zionhuang.music.constants.SkipSilenceKey
 import com.zionhuang.music.constants.StopMusicOnTaskClearKey
@@ -38,11 +39,12 @@ fun PlayerSettings(
     navController: NavController,
     scrollBehavior: TopAppBarScrollBehavior,
 ) {
-    val (audioQuality, onAudioQualityChange) = rememberEnumPreference(key = AudioQualityKey, defaultValue = AudioQuality.AUTO)
-    val (persistentQueue, onPersistentQueueChange) = rememberPreference(key = PersistentQueueKey, defaultValue = true)
-    val (skipSilence, onSkipSilenceChange) = rememberPreference(key = SkipSilenceKey, defaultValue = false)
-    val (audioNormalization, onAudioNormalizationChange) = rememberPreference(key = AudioNormalizationKey, defaultValue = true)
-    val (stopMusicOnTaskClear, onStopMusicOnTaskClearChange) = rememberPreference(key = StopMusicOnTaskClearKey, defaultValue = false)
+    val (audioQuality, onAudioQualityChange) = rememberEnumPreference(AudioQualityKey, defaultValue = AudioQuality.AUTO)
+    val (persistentQueue, onPersistentQueueChange) = rememberPreference(PersistentQueueKey, defaultValue = true)
+    val (skipSilence, onSkipSilenceChange) = rememberPreference(SkipSilenceKey, defaultValue = false)
+    val (audioNormalization, onAudioNormalizationChange) = rememberPreference(AudioNormalizationKey, defaultValue = true)
+    val (autoSkipNextOnError, onAutoSkipNextOnErrorChange) = rememberPreference(AutoSkipNextOnErrorKey, defaultValue = false)
+    val (stopMusicOnTaskClear, onStopMusicOnTaskClearChange) = rememberPreference(StopMusicOnTaskClearKey, defaultValue = false)
 
     Column(
         Modifier
@@ -66,13 +68,14 @@ fun PlayerSettings(
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.persistent_queue)) },
+            description = stringResource(R.string.persistent_queue_desc),
             icon = { Icon(painterResource(R.drawable.queue_music), null) },
             checked = persistentQueue,
             onCheckedChange = onPersistentQueueChange
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.skip_silence)) },
-            icon = { Icon(painterResource(R.drawable.skip_next), null) },
+            icon = { Icon(painterResource(R.drawable.fast_forward), null) },
             checked = skipSilence,
             onCheckedChange = onSkipSilenceChange
         )
@@ -81,6 +84,13 @@ fun PlayerSettings(
             icon = { Icon(painterResource(R.drawable.volume_up), null) },
             checked = audioNormalization,
             onCheckedChange = onAudioNormalizationChange
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.auto_skip_next_on_error)) },
+            description = stringResource(R.string.auto_skip_next_on_error_desc),
+            icon = { Icon(painterResource(R.drawable.skip_next), null) },
+            checked = autoSkipNextOnError,
+            onCheckedChange = onAutoSkipNextOnErrorChange
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.stop_music_on_task_clear)) },

mostafaalagamy/Metrolist

app/src/main/java/com/metrolist/music/playback/MusicService.kt
@@ -50,6 +50,7 @@ import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
 import androidx.media3.session.SessionToken
 import com.google.common.util.concurrent.MoreExecutors
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/playback/MusicService.kt
 import com.metrolist.innertube.YouTube
 import com.metrolist.innertube.models.SongItem
 import com.metrolist.innertube.models.WatchEndpoint
@@ -101,6 +102,60 @@ import com.metrolist.music.utils.dataStore
 import com.metrolist.music.utils.enumPreference
 import com.metrolist.music.utils.get
 import com.metrolist.music.utils.reportException
+=======
+import com.metrolist.innertube.YouTube
+import com.metrolist.innertube.models.SongItem
+import com.metrolist.innertube.models.WatchEndpoint
+import com.metrolist.innertube.models.response.PlayerResponse
+import com.metrolist.music.MainActivity
+import com.metrolist.music.R
+import com.metrolist.music.constants.AudioNormalizationKey
+import com.metrolist.music.constants.AudioQuality
+import com.metrolist.music.constants.AudioQualityKey
+import com.metrolist.music.constants.AutoSkipNextOnErrorKey
+import com.metrolist.music.constants.DiscordTokenKey
+import com.metrolist.music.constants.EnableDiscordRPCKey
+import com.metrolist.music.constants.HideExplicitKey
+import com.metrolist.music.constants.HistoryDuration
+import com.metrolist.music.constants.MediaSessionConstants.CommandToggleLike
+import com.metrolist.music.constants.MediaSessionConstants.CommandToggleRepeatMode
+import com.metrolist.music.constants.MediaSessionConstants.CommandToggleShuffle
+import com.metrolist.music.constants.PauseListenHistoryKey
+import com.metrolist.music.constants.PersistentQueueKey
+import com.metrolist.music.constants.PlayerVolumeKey
+import com.metrolist.music.constants.RepeatModeKey
+import com.metrolist.music.constants.ShowLyricsKey
+import com.metrolist.music.constants.SkipSilenceKey
+import com.metrolist.music.db.MusicDatabase
+import com.metrolist.music.db.entities.Event
+import com.metrolist.music.db.entities.FormatEntity
+import com.metrolist.music.db.entities.LyricsEntity
+import com.metrolist.music.db.entities.RelatedSongMap
+import com.metrolist.music.di.DownloadCache
+import com.metrolist.music.di.PlayerCache
+import com.metrolist.music.extensions.SilentHandler
+import com.metrolist.music.extensions.collect
+import com.metrolist.music.extensions.collectLatest
+import com.metrolist.music.extensions.currentMetadata
+import com.metrolist.music.extensions.findNextMediaItemById
+import com.metrolist.music.extensions.mediaItems
+import com.metrolist.music.extensions.metadata
+import com.metrolist.music.extensions.toMediaItem
+import com.metrolist.music.lyrics.LyricsHelper
+import com.metrolist.music.models.PersistQueue
+import com.metrolist.music.models.toMediaMetadata
+import com.metrolist.music.playback.queues.EmptyQueue
+import com.metrolist.music.playback.queues.ListQueue
+import com.metrolist.music.playback.queues.Queue
+import com.metrolist.music.playback.queues.YouTubeQueue
+import com.metrolist.music.playback.queues.filterExplicit
+import com.metrolist.music.utils.CoilBitmapLoader
+import com.metrolist.music.utils.DiscordRPC
+import com.metrolist.music.utils.dataStore
+import com.metrolist.music.utils.enumPreference
+import com.metrolist.music.utils.get
+import com.metrolist.music.utils.reportException
+>>>>>>> afebaa28 (feat: auto skip next on error):app/src/main/java/com/metrolist/music/playback/MusicService.kt
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -155,7 +210,7 @@ class MusicService :
 
     private lateinit var connectivityManager: ConnectivityManager
 
-    private val audioQuality by enumPreference(this, AudioQualityKey, AudioQuality.AUTO)
+    private val audioQuality by enumPreference(this, AudioQualityKey, com.metrolist.music.constants.AudioQuality.AUTO)
 
     private var currentQueue: Queue = EmptyQueue
     var queueTitle: String? = null
@@ -687,6 +742,14 @@ class MusicService :
             dataStore.edit { settings ->
                 settings[RepeatModeKey] = repeatMode
             }
+        }
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        if (dataStore.get(AutoSkipNextOnErrorKey, false) && player.hasNextMediaItem()) {
+            player.seekToNext()
+            player.prepare()
+            player.playWhenReady = true
         }
     }
 

app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
 import com.metrolist.music.LocalPlayerAwareWindowInsets
 import com.metrolist.music.R
 import com.metrolist.music.constants.AudioNormalizationKey
@@ -30,6 +31,23 @@ import com.metrolist.music.ui.component.SwitchPreference
 import com.metrolist.music.ui.utils.backToMain
 import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
+=======
+import com.metrolist.music.LocalPlayerAwareWindowInsets
+import com.metrolist.music.R
+import com.metrolist.music.constants.AudioNormalizationKey
+import com.metrolist.music.constants.AudioQuality
+import com.metrolist.music.constants.AudioQualityKey
+import com.metrolist.music.constants.AutoSkipNextOnErrorKey
+import com.metrolist.music.constants.PersistentQueueKey
+import com.metrolist.music.constants.SkipSilenceKey
+import com.metrolist.music.constants.StopMusicOnTaskClearKey
+import com.metrolist.music.ui.component.EnumListPreference
+import com.metrolist.music.ui.component.IconButton
+import com.metrolist.music.ui.component.SwitchPreference
+import com.metrolist.music.ui.utils.backToMain
+import com.metrolist.music.utils.rememberEnumPreference
+import com.metrolist.music.utils.rememberPreference
+>>>>>>> afebaa28 (feat: auto skip next on error):app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,10 +55,19 @@ fun PlayerSettings(
     navController: NavController,
     scrollBehavior: TopAppBarScrollBehavior,
 ) {
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
     val (audioQuality, onAudioQualityChange) = rememberEnumPreference(key = AudioQualityKey, defaultValue = AudioQuality.AUTO)
     val (persistentQueue, onPersistentQueueChange) = rememberPreference(key = PersistentQueueKey, defaultValue = true)
     val (skipSilence, onSkipSilenceChange) = rememberPreference(key = SkipSilenceKey, defaultValue = false)
     val (audioNormalization, onAudioNormalizationChange) = rememberPreference(key = AudioNormalizationKey, defaultValue = true)
+=======
+    val (audioQuality, onAudioQualityChange) = rememberEnumPreference(AudioQualityKey, defaultValue = AudioQuality.AUTO)
+    val (persistentQueue, onPersistentQueueChange) = rememberPreference(PersistentQueueKey, defaultValue = true)
+    val (skipSilence, onSkipSilenceChange) = rememberPreference(SkipSilenceKey, defaultValue = false)
+    val (audioNormalization, onAudioNormalizationChange) = rememberPreference(AudioNormalizationKey, defaultValue = true)
+    val (autoSkipNextOnError, onAutoSkipNextOnErrorChange) = rememberPreference(AutoSkipNextOnErrorKey, defaultValue = false)
+    val (stopMusicOnTaskClear, onStopMusicOnTaskClearChange) = rememberPreference(StopMusicOnTaskClearKey, defaultValue = false)
+>>>>>>> afebaa28 (feat: auto skip next on error):app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
 
     Column(
         Modifier
@@ -64,13 +91,14 @@ fun PlayerSettings(
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.persistent_queue)) },
+            description = stringResource(R.string.persistent_queue_desc),
             icon = { Icon(painterResource(R.drawable.queue_music), null) },
             checked = persistentQueue,
             onCheckedChange = onPersistentQueueChange,
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.skip_silence)) },
-            icon = { Icon(painterResource(R.drawable.skip_next), null) },
+            icon = { Icon(painterResource(R.drawable.fast_forward), null) },
             checked = skipSilence,
             onCheckedChange = onSkipSilenceChange,
         )
@@ -80,6 +108,22 @@ fun PlayerSettings(
             checked = audioNormalization,
             onCheckedChange = onAudioNormalizationChange,
         )
+<<<<<<< HEAD:app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
+=======
+        SwitchPreference(
+            title = { Text(stringResource(R.string.auto_skip_next_on_error)) },
+            description = stringResource(R.string.auto_skip_next_on_error_desc),
+            icon = { Icon(painterResource(R.drawable.skip_next), null) },
+            checked = autoSkipNextOnError,
+            onCheckedChange = onAutoSkipNextOnErrorChange,
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.stop_music_on_task_clear)) },
+            icon = { Icon(painterResource(R.drawable.clear_all), null) },
+            checked = stopMusicOnTaskClear,
+            onCheckedChange = onStopMusicOnTaskClearChange,
+        )
+>>>>>>> afebaa28 (feat: auto skip next on error):app/src/main/java/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
     }
 
     TopAppBar(