3a887aedb15112857efdd5395ff52c2ef719685d
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-15 16:47:19 +0300

    Update Player.kt

--- app/src/main/java/com/metrolist/music/ui/player/Player.kt
+++ app/src/main/java/com/metrolist/music/ui/player/Player.kt
@@ -49,6 +49,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -571,8 +572,8 @@ fun BottomSheetPlayer(
             } else {
                 Brush.verticalGradient(
                     listOf(
-                        MaterialTheme.colorScheme.surfaceContainer,
-                        MaterialTheme.colorScheme.surfaceContainer,
+                        MaterialTheme.colorScheme.surfaceColorAtElevation(6.dp),
+                        MaterialTheme.colorScheme.surfaceColorAtElevation(6.dp),
                     ),
                 )
             },
