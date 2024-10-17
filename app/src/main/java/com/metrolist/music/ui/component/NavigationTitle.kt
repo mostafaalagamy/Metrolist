ec4becc51c742a9d10e9560b1c1a00e06700981b
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-18 00:02:20 +0300

    Update NavigationTitle.kt

--- app/src/main/java/com/metrolist/music/ui/component/NavigationTitle.kt
+++ app/src/main/java/com/metrolist/music/ui/component/NavigationTitle.kt
@@ -1,6 +1,8 @@
 package com.metrolist.music.ui.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.metrolist.music.R
 
@@ -24,10 +27,13 @@ import com.metrolist.music.R
 fun NavigationTitle(
     title: String,
     modifier: Modifier = Modifier,
+    label: String? = null,
+    thumbnail: (@Composable () -> Unit)? = null,
     onClick: (() -> Unit)? = null,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
         modifier =
             modifier
                 .fillMaxWidth()
@@ -36,13 +42,27 @@ fun NavigationTitle(
                     onClick?.invoke()
                 }.padding(horizontal = 12.dp, vertical = 16.dp),
     ) {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary,
+        thumbnail?.invoke()
+        Column(
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier.weight(1f),
-        )
+        ) {
+            label?.let { label ->
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.labelLarge,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+        }
 
         if (onClick != null) {
             Icon(
