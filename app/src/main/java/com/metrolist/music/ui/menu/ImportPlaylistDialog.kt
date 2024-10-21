38681350d1fa91506202d7fabf302dfbd4942f29
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-21 05:05:13 +0300

    Create ImportPlaylistDialog.kt

---
+++ app/src/main/java/com/metrolist/music/ui/menu/ImportPlaylistDialog.kt
@@ -0,0 +1,63 @@
+package com.metrolist.music.ui.menu
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import com.metrolist.music.LocalDatabase
+import com.metrolist.music.R
+import com.metrolist.music.db.entities.PlaylistEntity
+import com.metrolist.music.ui.component.TextFieldDialog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+@Composable
+fun ImportPlaylistDialog(
+    isVisible: Boolean,
+    onGetSong: suspend () -> List<String>, // list of song ids. Songs should be inserted to database in this function.
+    playlistTitle: String,
+    onDismiss: () -> Unit,
+) {
+    val database = LocalDatabase.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val textFieldValue by remember { mutableStateOf(TextFieldValue(text = playlistTitle)) }
+    var songIds by remember {
+        mutableStateOf<List<String>?>(null) // list is not saveable
+    }
+
+    if (isVisible) {
+        TextFieldDialog(
+            icon = { Icon(painter = painterResource(R.drawable.add), contentDescription = null) },
+            title = { Text(text = stringResource(R.string.import_playlist)) },
+            initialTextFieldValue = textFieldValue,
+            autoFocus = false,
+            onDismiss = onDismiss,
+            onDone = { finalName ->
+                val newPlaylist = PlaylistEntity(
+                    name = finalName
+                )
+                database.query { insert(newPlaylist) }
+
+                coroutineScope.launch(Dispatchers.IO) {
+                    val playlist = database.playlist(newPlaylist.id).firstOrNull()
+
+                    if (playlist != null){
+                        songIds = onGetSong()
+                        database.addSongToPlaylist(playlist, songIds!!)
+                    }
+
+                    onDismiss()
+                }
+            }
+        )
+    }
+}
