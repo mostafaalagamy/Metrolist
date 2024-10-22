245a60e3d6769bd9fe950ac502a73c451c522114
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-22 20:03:39 +0300

    feat: add artists in Accounts page

--- innertube/src/main/java/com/metrolist/innertube/YouTube.kt
+++ innertube/src/main/java/com/metrolist/innertube/YouTube.kt
@@ -7,6 +7,7 @@ import com.metrolist.innertube.models.ArtistItem
 import com.metrolist.innertube.models.BrowseEndpoint
 import com.metrolist.innertube.models.GridRenderer
 import com.metrolist.innertube.models.MusicResponsiveListItemRenderer
+import com.metrolist.innertube.models.MusicShelfRenderer
 import com.metrolist.innertube.models.MusicTwoRowItemRenderer
 import com.metrolist.innertube.models.PlaylistItem
 import com.metrolist.innertube.models.SearchSuggestions
@@ -864,16 +865,49 @@ object YouTube {
                 }
         }
 
-    suspend fun libraryAlbums(): Result<List<AlbumItem>> = runCatching {
-        val response = innerTube.browse(
-            client = WEB_REMIX,
-            browseId = "FEmusic_liked_albums",
-            setLogin = true
-        ).body<BrowseResponse>()
-        response.contents?.singleColumnBrowseResultsRenderer?.tabs?.firstOrNull()?.tabRenderer?.content?.sectionListRenderer?.contents?.firstOrNull()?.gridRenderer?.items!!
+    suspend fun libraryAlbums(): Result<List<AlbumItem>> =
+        runCatching {
+            val response = innerTube.browse(
+                client = WEB_REMIX,
+                browseId = "FEmusic_liked_albums",
+                setLogin = true
+            ).body<BrowseResponse>()
+        response.contents
+            ?.singleColumnBrowseResultsRenderer
+            ?.tabs
+            ?.firstOrNull()
+            ?.tabRenderer
+            ?.content
+            ?.sectionListRenderer
+            ?.contents
+            ?.firstOrNull()
+            ?.gridRenderer
+            ?.items!!
             .mapNotNull(GridRenderer.Item::musicTwoRowItemRenderer)
             .mapNotNull {
                 ArtistItemsPage.fromMusicTwoRowItemRenderer(it) as? AlbumItem
+            }
+    }
+
+    suspend fun libraryArtistsSubscriptions(): Result<List<ArtistItem>> =
+        runCatching {
+            val response = innerTube.browse(
+                client = WEB_REMIX,
+                browseId = "FEmusic_library_corpus_artists",
+                setLogin = true
+            ).body<BrowseResponse>()
+        response.contents
+            ?.singleColumnBrowseResultsRenderer
+            ?.tabs?.firstOrNull()
+            ?.tabRenderer
+            ?.content
+            ?.sectionListRenderer
+            ?.contents?.firstOrNull()
+            ?.musicShelfRenderer
+            ?.contents!!
+            .mapNotNull(MusicShelfRenderer.Content::musicResponsiveListItemRenderer)
+            .mapNotNull {
+                SearchSuggestionPage.fromMusicResponsiveListItemRenderer(it) as? ArtistItem
             }
     }
 
