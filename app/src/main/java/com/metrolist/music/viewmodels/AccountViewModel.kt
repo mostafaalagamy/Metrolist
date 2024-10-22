a7a7a9a88266960088c514372c154dafa7b46809
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-22 18:03:07 +0300

    feat: show saved albums in the Account page

--- app/src/main/java/com/metrolist/music/viewmodels/AccountViewModel.kt
+++ app/src/main/java/com/metrolist/music/viewmodels/AccountViewModel.kt
@@ -3,6 +3,7 @@ package com.metrolist.music.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.metrolist.innertube.YouTube
+import com.metrolist.innertube.models.AlbumItem
 import com.metrolist.innertube.models.PlaylistItem
 import com.metrolist.music.utils.reportException
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,11 +14,17 @@ import javax.inject.Inject
 @HiltViewModel
 class AccountViewModel @Inject constructor() : ViewModel() {
     val playlists = MutableStateFlow<List<PlaylistItem>?>(null)
+    val albums = MutableStateFlow<List<AlbumItem>?>(null)
 
     init {
         viewModelScope.launch {
             YouTube.likedPlaylists().onSuccess {
                 playlists.value = it
+            }.onFailure {
+                reportException(it)
+            }
+             YouTube.libraryAlbums().onSuccess {
+                albums.value = it
             }.onFailure {
                 reportException(it)
             }
