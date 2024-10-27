97567a21f1d5e93a000f78848a398ad8283359ee
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-10-27 04:52:47 +0300

    feat: support loading premium exclusive albums for premium accounts

--- innertube/src/main/java/com/metrolist/innertube/InnerTube.kt
+++ innertube/src/main/java/com/metrolist/innertube/InnerTube.kt
@@ -63,6 +63,8 @@ class InnerTube {
             httpClient = createClient()
         }
 
+    var useLoginForBrowse: Boolean = false
+
     @OptIn(ExperimentalSerializationApi::class)
     private fun createClient() =
         HttpClient(OkHttp) {
@@ -129,7 +131,7 @@ class InnerTube {
         params: String? = null,
         continuation: String? = null,
     ) = httpClient.post("search") {
-        ytClient(client)
+        ytClient(client, setLogin = useLoginForBrowse)
         setBody(
             SearchBody(
                 context = client.toContext(locale, visitorData),
@@ -180,7 +182,7 @@ class InnerTube {
         continuation: String? = null,
         setLogin: Boolean = false,
     ) = httpClient.post("browse") {
-        ytClient(client, setLogin)
+        ytClient(client, setLogin = setLogin || useLoginForBrowse)
         setBody(
             BrowseBody(
                 context = client.toContext(locale, visitorData),
