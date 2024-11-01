27d49bca6c2024607403f5ec4dd9ffdcd07c6e6c
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-01 21:14:43 +0200

    Bump: Bump to v10.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.metrolist.music"
         minSdk = 24
         targetSdk = 35
-        versionCode = 90
-        versionName = "9.9.9"
+        versionCode = 100
+        versionName = "10.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
