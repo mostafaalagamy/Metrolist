073c8b63ff87eb746648ac9a1fcbdaa055da057a
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-04 19:26:55 +0300

    Add files via upload

--- app/src/main/java/com/metrolist/music/MainActivity.kt
+++ app/src/main/java/com/metrolist/music/MainActivity.kt
@@ -67,7 +67,6 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastForEach
@@ -96,7 +95,6 @@ import com.metrolist.music.constants.PauseSearchHistoryKey
 import com.metrolist.music.constants.PureBlackKey
 import com.metrolist.music.constants.SearchSource
 import com.metrolist.music.constants.SearchSourceKey
-import com.metrolist.music.constants.SlimNavBarKey
 import com.metrolist.music.db.MusicDatabase
 import com.metrolist.music.db.entities.SearchHistory
 import com.metrolist.music.extensions.*
@@ -274,7 +272,6 @@ class MainActivity : ComponentActivity() {
                     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
                     val navigationItems = remember { Screens.MainScreens }
-		    val (slimNav) = rememberPreference(SlimNavBarKey, defaultValue = false)
                     val defaultOpenTab =
                         remember {
                             dataStore[DefaultOpenTabKey].toEnum(defaultValue = NavigationTab.HOME)
@@ -344,15 +341,6 @@ class MainActivity : ComponentActivity() {
                                 navigationItems.fastAny { it.route == navBackStackEntry?.destination?.route } &&
                                 !active
                         }
-
-			fun getNavPadding(): Dp {
-                        return if (shouldShowNavigationBar) {
-                            if (slimNav) 48.dp else 64.dp
-                        } else {
-                            0.dp
-                        }
-			}
-			
                     val navigationBarHeight by animateDpAsState(
                         targetValue = if (shouldShowNavigationBar) NavigationBarHeight else 0.dp,
                         animationSpec = NavigationBarAnimationSpec,
@@ -362,7 +350,7 @@ class MainActivity : ComponentActivity() {
                     val playerBottomSheetState =
                         rememberBottomSheetState(
                             dismissedBound = 0.dp,
-                            collapsedBound = bottomInset + getNavPadding() + MiniPlayerHeight + 4.dp,
+                            collapsedBound = bottomInset + (if (shouldShowNavigationBar) NavigationBarHeight else 0.dp) + MiniPlayerHeight,
                             expandedBound = maxHeight,
                         )
 
@@ -787,7 +775,6 @@ class MainActivity : ComponentActivity() {
                             modifier =
                                 Modifier
                                     .align(Alignment.BottomCenter)
-				    .height(bottomInset + getNavPadding())
                                     .offset {
                                         if (navigationBarHeight == 0.dp) {
                                             IntOffset(
@@ -821,13 +808,11 @@ class MainActivity : ComponentActivity() {
                                         )
                                     },
                                     label = {
-                                        if (!slimNav) {
-                                            Text(
-                                                text = stringResource(screen.titleId),
-                                                maxLines = 1,
-                                                overflow = TextOverflow.Ellipsis
-                                            )
-                                        }
+                                        Text(
+                                            text = stringResource(screen.titleId),
+                                            maxLines = 1,
+                                            overflow = TextOverflow.Ellipsis,
+                                        )
                                     },
                                     onClick = {
                                         if (navBackStackEntry?.destination?.hierarchy?.any { it.route == screen.route } == true) {
