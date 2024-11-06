82055a8e68de256c015961b05c9a56f407e7989b
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-06 15:26:26 +0200

    Update GridMenu.kt

--- app/src/main/java/com/metrolist/music/ui/component/GridMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/component/GridMenu.kt
@@ -112,7 +112,7 @@ fun LazyGridScope.DownloadGridMenu(
     when (state) {
         Download.STATE_COMPLETED -> {
             GridMenuItem(
-                icon = R.drawable.offline_pin,
+                icon = R.drawable.offline,
                 title = R.string.remove_download,
                 onClick = onRemoveDownload,
             )
