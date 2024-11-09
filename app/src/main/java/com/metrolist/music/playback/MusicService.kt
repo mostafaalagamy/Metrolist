9d55f73dd1d85ccb7bec2b2a5f3d47960a00481e
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-09 20:14:38 +0200

    Revert "fix: unable to download songs and video songs not playing"
    This reverts commit 397968d8db07635dcffbb3beea85a97fd7c5aeb9.

--- app/src/main/java/com/metrolist/music/playback/MusicService.kt
+++ app/src/main/java/com/metrolist/music/playback/MusicService.kt
@@ -824,8 +824,19 @@ class MusicService :
                 )
             }
             scope.launch(Dispatchers.IO) { recoverSong(mediaId, playerResponse) }
-            songUrlCache[mediaId] = format.findUrl() to playerResponse.streamingData!!.expiresInSeconds * 1000L
-            dataSpec.withUri(format.findUrl().toUri()).subrange(dataSpec.uriPositionOffset, CHUNK_LENGTH)
+            val url =
+                run {
+                    runBlocking(Dispatchers.IO) {
+                        format.findUrl()
+                    }
+                }
+
+            songUrlCache[mediaId] = url to (
+                playerResponse.streamingData?.expiresInSeconds?.times(
+                    1000L,
+                ) ?: 0
+            )
+            dataSpec.withUri(url.toUri()).subrange(dataSpec.uriPositionOffset, CHUNK_LENGTH)
         }
     }
 
