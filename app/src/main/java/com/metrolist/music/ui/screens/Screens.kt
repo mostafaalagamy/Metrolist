28a77d9bde50c1d1b6efe32e2bfaff6f10e51359
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-12 03:38:49 +0200

    Update Screens.kt

--- app/src/main/java/com/metrolist/music/ui/screens/Screens.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/Screens.kt
@@ -8,14 +8,30 @@ import com.metrolist.music.R
 @Immutable
 sealed class Screens(
     @StringRes val titleId: Int,
-    @DrawableRes val iconId: Int,
+    @DrawableRes val iconIdInactive: Int,
+    @DrawableRes val iconIdActive: Int,
     val route: String,
 ) {
-    object Home : Screens(R.string.home, R.drawable.home, "home")
+    object Home : Screens(
+        titleId = R.string.home,
+        iconIdInactive = R.drawable.home_outlined,
+        iconIdActive = R.drawable.home_filled,
+        route = "home"
+    )
 
-    object Explore : Screens(R.string.explore, R.drawable.explore, "explore")
+    object Explore : Screens(
+        titleId = R.string.explore,
+        iconIdInactive = R.drawable.explore_outlined,
+        iconIdActive = R.drawable.explore_filled,
+        route = "explore"
+    )
 
-    object Library : Screens(R.string.filter_library, R.drawable.library_music, "library")
+    object Library : Screens(
+        titleId = R.string.filter_library,
+        iconIdInactive = R.drawable.library_music_outlined,
+        iconIdActive = R.drawable.library_music_filled,
+        route = "library"
+    )
 
     companion object {
         val MainScreens = listOf(Home, Explore, Library)
