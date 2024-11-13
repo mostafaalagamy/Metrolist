330bbdfd30e99cf97a3dcd4e03a4697461c18eab
Mostafa Al Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-13 17:58:33 +0200

    Update TopPlaylistScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/playlist/TopPlaylistScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/playlist/TopPlaylistScreen.kt
@@ -419,7 +419,7 @@ fun TopPlaylistScreen(
                         if (selection) {
                             val count = wrappedSongs?.count { it.isSelected }
                             Text(
-                                text = stringResource(R.string.elements_selected, count),
+                                text = stringResource(R.string.elements_selected, count ?: 0),
                                 modifier = Modifier.weight(1f))
                             IconButton(
                                 onClick = {
