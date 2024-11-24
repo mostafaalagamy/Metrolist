a053bc83fbdd6988167385f524f7fea154c4613b
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-24 14:45:57 +0200

    Update PlaylistMenu.kt

--- app/src/main/java/com/metrolist/music/ui/menu/PlaylistMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/PlaylistMenu.kt
@@ -56,16 +56,12 @@ import com.metrolist.music.utils.makeTimeString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.time.LocalDateTime
 
 @Composable
 fun PlaylistMenu(
     playlist: Playlist,
     coroutineScope: CoroutineScope,
     onDismiss: () -> Unit,
-    autoPlaylist: Boolean? = false,
-    downloadPlaylist: Boolean? = false,
-    songList: List<Song>? = emptyList(),
 ) {
     val context = LocalContext.current
     val database = LocalDatabase.current
@@ -74,20 +70,13 @@ fun PlaylistMenu(
     var songs by remember {
         mutableStateOf(emptyList<Song>())
     }
-    val playlistLength =
-        remember(songs) {
-            songs.fastSumBy { it.song.duration }
-        }
+    val playlistLength = remember(songs) {
+        songs.fastSumBy { it.song.duration }
+    }
 
     LaunchedEffect(Unit) {
-        if (autoPlaylist == false) {
-            database.playlistSongs(playlist.id).collect {
-                songs = it.map(PlaylistSong::song)
-            }
-        } else {
-            if (songList != null) {
-                songs = songList
-            }
+        database.playlistSongs(playlist.id).collect {
+            songs = it.map(PlaylistSong::song)
         }
     }
 
@@ -99,18 +88,16 @@ fun PlaylistMenu(
         if (songs.isEmpty()) return@LaunchedEffect
         downloadUtil.downloads.collect { downloads ->
             downloadState =
-                if (songs.all { downloads[it.id]?.state == Download.STATE_COMPLETED }) {
+                if (songs.all { downloads[it.id]?.state == Download.STATE_COMPLETED })
                     Download.STATE_COMPLETED
-                } else if (songs.all {
-                        downloads[it.id]?.state == Download.STATE_QUEUED ||
-                            downloads[it.id]?.state == Download.STATE_DOWNLOADING ||
-                            downloads[it.id]?.state == Download.STATE_COMPLETED
-                    }
-                ) {
+                else if (songs.all {
+                        downloads[it.id]?.state == Download.STATE_QUEUED
+                                || downloads[it.id]?.state == Download.STATE_DOWNLOADING
+                                || downloads[it.id]?.state == Download.STATE_COMPLETED
+                    })
                     Download.STATE_DOWNLOADING
-                } else {
+                else
                     Download.STATE_STOPPED
-                }
         }
     }
 
@@ -123,17 +110,16 @@ fun PlaylistMenu(
             icon = { Icon(painter = painterResource(R.drawable.edit), contentDescription = null) },
             title = { Text(text = stringResource(R.string.edit_playlist)) },
             onDismiss = { showEditDialog = false },
-            initialTextFieldValue =
-                TextFieldValue(
-                    playlist.playlist.name,
-                    TextRange(playlist.playlist.name.length),
-                ),
+            initialTextFieldValue = TextFieldValue(
+                playlist.playlist.name,
+                TextRange(playlist.playlist.name.length)
+            ),
             onDone = { name ->
                 onDismiss()
                 database.query {
-                    update(playlist.playlist.copy(name = name, lastUpdateTime = LocalDateTime.now()))
+                    update(playlist.playlist.copy(name = name))
                 }
-            },
+            }
         )
     }
 
@@ -148,14 +134,14 @@ fun PlaylistMenu(
                 Text(
                     text = stringResource(R.string.remove_download_playlist_confirm, playlist.playlist.name),
                     style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(horizontal = 18.dp),
+                    modifier = Modifier.padding(horizontal = 18.dp)
                 )
             },
             buttons = {
                 TextButton(
                     onClick = {
                         showRemoveDownloadDialog = false
-                    },
+                    }
                 ) {
                     Text(text = stringResource(android.R.string.cancel))
                 }
@@ -168,14 +154,14 @@ fun PlaylistMenu(
                                 context,
                                 ExoDownloadService::class.java,
                                 song.song.id,
-                                false,
+                                false
                             )
                         }
-                    },
+                    }
                 ) {
                     Text(text = stringResource(android.R.string.ok))
                 }
-            },
+            }
         )
     }
 
@@ -190,14 +176,14 @@ fun PlaylistMenu(
                 Text(
                     text = stringResource(R.string.delete_playlist_confirm, playlist.playlist.name),
                     style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(horizontal = 18.dp),
+                    modifier = Modifier.padding(horizontal = 18.dp)
                 )
             },
             buttons = {
                 TextButton(
                     onClick = {
                         showDeletePlaylistDialog = false
-                    },
+                    }
                 ) {
                     Text(text = stringResource(android.R.string.cancel))
                 }
@@ -209,11 +195,11 @@ fun PlaylistMenu(
                         database.query {
                             delete(playlist.playlist)
                         }
-                    },
+                    }
                 ) {
                     Text(text = stringResource(android.R.string.ok))
                 }
-            },
+            }
         )
     }
 
@@ -225,103 +211,90 @@ fun PlaylistMenu(
                 fontSize = 14.sp,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(end = 12.dp),
+                modifier = Modifier.padding(end = 12.dp)
             )
-        },
+        }
     )
 
     HorizontalDivider()
 
     GridMenu(
-        contentPadding =
-            PaddingValues(
-                start = 8.dp,
-                top = 8.dp,
-                end = 8.dp,
-                bottom = 8.dp + WindowInsets.systemBars.asPaddingValues().calculateBottomPadding(),
-            ),
+        contentPadding = PaddingValues(
+            start = 8.dp,
+            top = 8.dp,
+            end = 8.dp,
+            bottom = 8.dp + WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()
+        )
     ) {
         GridMenuItem(
             icon = R.drawable.play,
-            title = R.string.play,
+            title = R.string.play
         ) {
             onDismiss()
-            playerConnection.playQueue(
-                ListQueue(
-                    title = playlist.playlist.name,
-                    items = songs.map { it.toMediaItem() },
-                ),
-            )
+            playerConnection.playQueue(ListQueue(
+                title = playlist.playlist.name,
+                items = songs.map { it.toMediaItem() }
+            ))
         }
 
         GridMenuItem(
             icon = R.drawable.shuffle,
-            title = R.string.shuffle,
+            title = R.string.shuffle
         ) {
             onDismiss()
-            playerConnection.playQueue(
-                ListQueue(
-                    title = playlist.playlist.name,
-                    items = songs.shuffled().map { it.toMediaItem() },
-                ),
-            )
+            playerConnection.playQueue(ListQueue(
+                title = playlist.playlist.name,
+                items = songs.shuffled().map { it.toMediaItem() }
+            ))
         }
 
         GridMenuItem(
             icon = R.drawable.queue_music,
-            title = R.string.add_to_queue,
+            title = R.string.add_to_queue
         ) {
             onDismiss()
             playerConnection.addToQueue(songs.map { it.toMediaItem() })
         }
 
-        if (autoPlaylist != true) {
-            GridMenuItem(
-                icon = R.drawable.edit,
-                title = R.string.edit,
-            ) {
-                showEditDialog = true
-            }
+        GridMenuItem(
+            icon = R.drawable.edit,
+            title = R.string.edit
+        ) {
+            showEditDialog = true
         }
 
-        if (downloadPlaylist != true) {
-            DownloadGridMenu(
-                state = downloadState,
-                onDownload = {
-                    songs.forEach { song ->
-                        val downloadRequest =
-                            DownloadRequest
-                                .Builder(song.id, song.id.toUri())
-                                .setCustomCacheKey(song.id)
-                                .setData(song.song.title.toByteArray())
-                                .build()
-                        DownloadService.sendAddDownload(
-                            context,
-                            ExoDownloadService::class.java,
-                            downloadRequest,
-                            false,
-                        )
-                    }
-                },
-                onRemoveDownload = {
-                    showRemoveDownloadDialog = true
-                },
-            )
-        }
-
-        if (autoPlaylist != true) {
-            GridMenuItem(
-                icon = R.drawable.delete,
-                title = R.string.delete,
-            ) {
-                showDeletePlaylistDialog = true
+        DownloadGridMenu(
+            state = downloadState,
+            onDownload = {
+                songs.forEach { song ->
+                    val downloadRequest = DownloadRequest.Builder(song.id, song.id.toUri())
+                        .setCustomCacheKey(song.id)
+                        .setData(song.song.title.toByteArray())
+                        .build()
+                    DownloadService.sendAddDownload(
+                        context,
+                        ExoDownloadService::class.java,
+                        downloadRequest,
+                        false
+                    )
+                }
+            },
+            onRemoveDownload = {
+                showRemoveDownloadDialog = true
             }
+        )
+
+        GridMenuItem(
+            icon = R.drawable.delete,
+            title = R.string.delete
+        ) {
+            showDeletePlaylistDialog = true
         }
 
         if (playlist.playlist.browseId != null) {
             GridMenuItem(
                 icon = R.drawable.sync,
-                title = R.string.sync,
+                title = R.string.sync
             ) {
                 onDismiss()
                 coroutineScope.launch(Dispatchers.IO) {
@@ -337,9 +310,10 @@ fun PlaylistMenu(
                                 PlaylistSongMap(
                                     songId = song.id,
                                     playlistId = playlist.id,
-                                    position = position,
+                                    position = position
                                 )
-                            }.forEach(::insert)
+                            }
+                            .forEach(::insert)
                     }
                 }
             }
