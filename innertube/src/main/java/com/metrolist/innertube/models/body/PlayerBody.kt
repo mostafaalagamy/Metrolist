35261d5afce3cd1dded43736794bc5040ee2811a
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-11-30 17:01:28 +0200

    Update PlayerBody.kt

--- innertube/src/main/java/com/metrolist/innertube/models/body/PlayerBody.kt
+++ innertube/src/main/java/com/metrolist/innertube/models/body/PlayerBody.kt
@@ -9,7 +9,7 @@ data class PlayerBody(
     val videoId: String,
     val playlistId: String?,
     val cpn: String? = "wzf9Y0nqz6AUe2Vr", // need some random cpn to get same algorithm for sig
-    val playbackContext: PlaybackContext? = PlaybackContext(ContentPlaybackContext(20019L)),
+    val playbackContext: PlaybackContext? = PlaybackContext(ContentPlaybackContext(20052L)),
 ) {
     @Serializable
     data class PlaybackContext(
