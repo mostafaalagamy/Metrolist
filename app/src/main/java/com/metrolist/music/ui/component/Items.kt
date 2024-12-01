fc03cf37d363b38eb62c960b535758094e1db337
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-01 22:51:39 +0200

    Update Items.kt

--- app/src/main/java/com/metrolist/music/ui/component/Items.kt
+++ app/src/main/java/com/metrolist/music/ui/component/Items.kt
@@ -745,6 +745,7 @@ fun AlbumListItem(
 @Composable
 fun AlbumGridItem(
     album: Album,
+    song: Song,
     modifier: Modifier = Modifier,
     coroutineScope: CoroutineScope,
     badges: @Composable RowScope.() -> Unit = {
@@ -1358,6 +1359,7 @@ fun YouTubeListItem(
 @Composable
 fun YouTubeGridItem(
     item: YTItem,
+    song: Song,
     modifier: Modifier = Modifier,
     coroutineScope: CoroutineScope? = null,
     badges: @Composable RowScope.() -> Unit = {
