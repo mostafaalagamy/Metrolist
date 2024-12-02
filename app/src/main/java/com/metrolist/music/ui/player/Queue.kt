3c3661ae02dc783159852059856ebf78c5b93dcf
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-03 01:26:36 +0200

    Update Queue.kt

--- app/src/main/java/com/metrolist/music/ui/player/Queue.kt
+++ app/src/main/java/com/metrolist/music/ui/player/Queue.kt
@@ -33,8 +33,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.MarqueeAnimationMode
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
