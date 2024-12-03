1d3bcb418756e84c2f3102307c3a3fa79997ba89
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-03 02:36:46 +0200

    Update Queue.kt

--- app/src/main/java/com/metrolist/music/ui/player/Queue.kt
+++ app/src/main/java/com/metrolist/music/ui/player/Queue.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
