0368684497ed281b0a2e0d54ef427a5c33872e88
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-18 01:28:44 +0200

    Update AccountSettings

--- app/src/main/java/com/metrolist/music/ui/screens/settings/AccountSettings.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/settings/AccountSettings.kt
@@ -28,6 +28,7 @@ import com.metrolist.music.constants.UseLoginForBrowse
 import com.metrolist.music.ui.component.IconButton
 import com.metrolist.music.ui.component.SwitchPreference
 import com.metrolist.music.ui.component.PreferenceEntry
+import com.metrolist.music.ui.component.PreferenceGroupTitle
 import com.metrolist.music.ui.utils.backToMain
 import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
