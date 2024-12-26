040c16def569a37837eecf38dc05334a11caab1b
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-26 17:21:44 +0200

    Update AlbumEntity.kt

--- app/src/main/java/com/metrolist/music/db/entities/AlbumEntity.kt
+++ app/src/main/java/com/metrolist/music/db/entities/AlbumEntity.kt
@@ -1,14 +1,21 @@
 package com.metrolist.music.db.entities
 
 import androidx.compose.runtime.Immutable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.metrolist.innertube.YouTube
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
 @Immutable
 @Entity(tableName = "album")
 data class AlbumEntity(
     @PrimaryKey val id: String,
+    val playlistId: String? = null,
     val title: String,
     val year: Int? = null,
     val thumbnailUrl: String? = null,
@@ -17,9 +24,17 @@ data class AlbumEntity(
     val duration: Int,
     val lastUpdateTime: LocalDateTime = LocalDateTime.now(),
     val bookmarkedAt: LocalDateTime? = null,
+    val likedDate: LocalDateTime? = null,
+    val inLibrary: LocalDateTime? = null,
 ) {
-    fun toggleLike() =
-        copy(
-            bookmarkedAt = if (bookmarkedAt != null) null else LocalDateTime.now(),
-        )
+    fun localToggleLike() = copy(
+        bookmarkedAt = if (bookmarkedAt != null) null else LocalDateTime.now()
+    )
+    fun toggleLike() = localToggleLike().also {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (playlistId != null)
+                YouTube.likePlaylist(playlistId, bookmarkedAt == null)
+            this.cancel()
+        }
+    }
 }
