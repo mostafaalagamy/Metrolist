7971d4fb91e1a7f09bb55efc888561c55f6dc930
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-28 15:14:48 +0200

    Update SongEntity.kt

--- app/src/main/java/com/metrolist/music/db/entities/SongEntity.kt
+++ app/src/main/java/com/metrolist/music/db/entities/SongEntity.kt
@@ -1,7 +1,6 @@
 package com.metrolist.music.db.entities
 
 import androidx.compose.runtime.Immutable
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
@@ -18,9 +17,9 @@ import java.time.LocalDateTime
     tableName = "song",
     indices = [
         Index(
-            value = ["albumId"]
-        )
-    ]
+            value = ["albumId"],
+        ),
+    ],
 )
 data class SongEntity(
     @PrimaryKey val id: String,
@@ -29,26 +28,34 @@ data class SongEntity(
     val thumbnailUrl: String? = null,
     val albumId: String? = null,
     val albumName: String? = null,
+    val year: Int? = null,
+    val date: LocalDateTime? = null, // ID3 tag property
+    val dateModified: LocalDateTime? = null, // file property
     val liked: Boolean = false,
+    val likedDate: LocalDateTime? = null,
     val totalPlayTime: Long = 0, // in milliseconds
     val inLibrary: LocalDateTime? = null,
+    val dateDownload: LocalDateTime? = null, // doubles as "isDownloaded"
 ) {
     fun localToggleLike() = copy(
         liked = !liked,
+        likedDate = if (!liked) LocalDateTime.now() else null,
+    )
+
+    fun toggleLike() = copy(
+        liked = !liked,
+        likedDate = if (!liked) LocalDateTime.now() else null,
         inLibrary = if (!liked) inLibrary ?: LocalDateTime.now() else inLibrary
     ).also {
-        CoroutineScope(Dispatchers.IO).launch() {
+        CoroutineScope(Dispatchers.IO).launch {
             YouTube.likeVideo(id, !liked)
             this.cancel()
         }
     }
 
-    fun toggleLike() = localToggleLike().also {
-        CoroutineScope(Dispatchers.IO).launch() {
-            YouTube.likeVideo(id, !liked)
-            this.cancel()
-        }
-    }
-
-    fun toggleLibrary() = copy(inLibrary = if (inLibrary == null) LocalDateTime.now() else null)
+    fun toggleLibrary() = copy(
+        inLibrary = if (inLibrary == null) LocalDateTime.now() else null,
+        liked = if (inLibrary == null) liked else false,
+        likedDate = if (inLibrary == null) likedDate else null
+    )
 }
