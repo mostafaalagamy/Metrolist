1aee4fb9418467867ec2f16f61e31d8d7a94ecd0
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-28 15:38:37 +0200

    Create LikeBody.kt

---
+++ innertube/src/main/java/com/metrolist/innertube/models/body/LikeBody.kt
@@ -0,0 +1,15 @@
+package com.metrolist.innertube.models.body
+
+import com.metrolist.innertube.models.Context
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LikeBody(
+    val context: Context,
+    val target: Target,
+) {
+    @Serializable
+    data class Target(
+        val videoId: String
+    )
+}
