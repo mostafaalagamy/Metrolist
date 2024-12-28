6166cf3100c2798e75387821c886760d76e50882
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-28 16:19:30 +0200

    Update SyncUtils.kt

--- app/src/main/java/com/metrolist/music/utils/SyncUtils.kt
+++ app/src/main/java/com/metrolist/music/utils/SyncUtils.kt
@@ -2,6 +2,7 @@ package com.metrolist.music.utils
 
 import com.metrolist.innertube.YouTube
 import com.metrolist.innertube.models.SongItem
+import com.metrolist.innertube.models.AlbumItem
 import com.metrolist.innertube.utils.completed
 import com.metrolist.music.db.MusicDatabase
 import com.metrolist.music.db.entities.SongEntity
@@ -26,6 +27,26 @@ class SyncUtils @Inject constructor(
                     when (dbSong) {
                         null -> insert(song.toMediaMetadata(), SongEntity::localToggleLike)
                         else -> if (!dbSong.song.liked) update(dbSong.song.localToggleLike())
+                    }
+                }
+            }
+        }
+    }
+    suspend fun syncLikedAlbums() {
+        YouTube.libraryAlbums().onSuccess { ytAlbums ->
+            database.albumsByNameAsc().first()
+                .filterNot { it.id in ytAlbums.map(AlbumItem::id) }
+                .forEach { database.update(it.album.localToggleLike()) }
+            ytAlbums.forEach { album ->
+                val dbAlbum = database.album(album.id).firstOrNull()
+                YouTube.album(album.browseId).onSuccess { albumPage ->
+                    when (dbAlbum) {
+                        null -> {
+                            database.insert(albumPage)
+                            database.album(album.id).firstOrNull()?.let { database.update(it.album) }
+                        }
+                        else -> if (dbAlbum.album.bookmarkedAt == null)
+                            database.update(dbAlbum.album.localToggleLike())
                     }
                 }
             }
