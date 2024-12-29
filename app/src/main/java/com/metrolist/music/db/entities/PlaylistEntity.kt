522ce01d2fbbbe5c104e09206f590af1b7097f29
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-29 21:03:33 +0200

    Update PlaylistEntity.kt

--- app/src/main/java/com/metrolist/music/db/entities/PlaylistEntity.kt
+++ app/src/main/java/com/metrolist/music/db/entities/PlaylistEntity.kt
@@ -1,8 +1,14 @@
 package com.metrolist.music.db.entities
 
 import androidx.compose.runtime.Immutable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.metrolist.innertube.YouTube
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import org.apache.commons.lang3.RandomStringUtils
 import java.time.LocalDateTime
 
@@ -12,6 +18,9 @@ data class PlaylistEntity(
     @PrimaryKey val id: String = generatePlaylistId(),
     val name: String,
     val browseId: String? = null,
+    @ColumnInfo(name = "isEditable", defaultValue = true.toString())
+    val isEditable: Boolean = true,
+    val bookmarkedAt: LocalDateTime? = null,
     val createdAt: LocalDateTime? = LocalDateTime.now(),
     val lastUpdateTime: LocalDateTime? = LocalDateTime.now(),
 ) {
@@ -20,5 +29,15 @@ data class PlaylistEntity(
         const val DOWNLOADED_PLAYLIST_ID = "LP_DOWNLOADED"
 
         fun generatePlaylistId() = "LP" + RandomStringUtils.random(8, true, false)
+    }
+    fun localToggleLike() = copy(
+        bookmarkedAt = if (bookmarkedAt != null) null else LocalDateTime.now()
+    )
+    fun toggleLike() = localToggleLike().also {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (browseId != null)
+                YouTube.likePlaylist(browseId, bookmarkedAt == null)
+            this.cancel()
+        }
     }
 }
