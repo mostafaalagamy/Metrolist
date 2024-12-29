3881e9a85656f72cfc16bbafd6baf2c4ba3d1354
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-29 22:46:37 +0200

    Update AddToPlaylistDialog.kt

--- app/src/main/java/com/metrolist/music/ui/menu/AddToPlaylistDialog.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/AddToPlaylistDialog.kt
@@ -2,10 +2,16 @@ package com.metrolist.music.ui.menu
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,6 +25,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import com.metrolist.innertube.YouTube
 import com.metrolist.music.LocalDatabase
 import com.metrolist.music.R
@@ -36,7 +46,9 @@ import java.time.LocalDateTime
 @Composable
 fun AddToPlaylistDialog(
     isVisible: Boolean,
-    onAdd: (Playlist) -> Unit,
+    noSyncing: Boolean = false,
+    initialTextFieldValue: String? = null,
+    onAdd: suspend (Playlist) -> List<String>, // list of song ids. Songs should be inserted to database in this function.
     onDismiss: () -> Unit,
 ) {
     val database = LocalDatabase.current
@@ -49,8 +61,12 @@ fun AddToPlaylistDialog(
         mutableStateOf(false)
     }
 
+    var syncedPlaylist: Boolean by remember {
+        mutableStateOf(false)
+    }
+
     LaunchedEffect(Unit) {
-        database.playlistsByCreateDateAsc().collect {
+        database.editablePlaylistsByCreateDateAsc().collect {
             playlists = it.asReversed()
         }
     }
@@ -80,11 +96,29 @@ fun AddToPlaylistDialog(
             items(playlists) { playlist ->
                 PlaylistListItem(
                     playlist = playlist,
-                    modifier =
-                        Modifier.clickable {
-                            onAdd(playlist)
-                            onDismiss()
-                        },
+                    modifier = Modifier.clickable {
+                        selectedPlaylist = playlist
+                        coroutineScope.launch(Dispatchers.IO) {
+                            if (songIds == null) {
+                                songIds = onAdd(playlist)
+                            }
+                            duplicates = database.playlistDuplicates(playlist.id, songIds!!)
+                            if (duplicates.isNotEmpty()) {
+                                showDuplicateDialog = true
+                            } else {
+                                onDismiss()
+                                database.addSongToPlaylist(playlist, songIds!!)
+                            }
+                        }
+                    }
+                )
+            }
+
+            item {
+                Text(
+                    text = "Note: Adding local songs to synced/remote playlists is unsupported. Any other combination is valid.",
+                    fontSize = TextUnit(12F, TextUnitType.Sp),
+                    modifier = Modifier.padding(horizontal = 20.dp)
                 )
             }
         }
@@ -94,21 +128,113 @@ fun AddToPlaylistDialog(
         TextFieldDialog(
             icon = { Icon(painter = painterResource(R.drawable.add), contentDescription = null) },
             title = { Text(text = stringResource(R.string.create_playlist)) },
+            initialTextFieldValue = TextFieldValue(initialTextFieldValue?: ""),
             onDismiss = { showCreatePlaylistDialog = false },
             onDone = { playlistName ->
                 coroutineScope.launch(Dispatchers.IO) {
-                    val browseId = YouTube.createPlaylist(playlistName).getOrNull()
+                    val browseId = if (syncedPlaylist)
+                        YouTube.createPlaylist(playlistName).getOrNull()
+                    else null
                     database.query {
                         insert(
                             PlaylistEntity(
                                 name = playlistName,
                                 browseId = browseId,
-                                bookmarkedAt = LocalDateTime.now()
+                                bookmarkedAt = LocalDateTime.now(),
+                                isEditable = !syncedPlaylist,
+                                isLocal = !syncedPlaylist // && check that all songs are non-local
                             )
+                        )
+                    }
+                }
+            },
+            extraContent = {
+                // synced/unsynced toggle
+                Row(
+                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 40.dp)
+                ) {
+                    Column() {
+                        Text(
+                            text = "Sync Playlist",
+                            style = MaterialTheme.typography.titleLarge,
+                        )
+                        Text(
+                            text = "Note: This allows for syncing with YouTube Music. This is NOT changeable later. You cannot add local songs to synced playlists.",
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.fillMaxWidth(0.7f)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Switch(
+                            enabled = !noSyncing,
+                            checked = syncedPlaylist,
+                            onCheckedChange = {
+                                syncedPlaylist = !syncedPlaylist
+                            },
                         )
                     }
                 }
             }
         )
+    }
+
+    if (showDuplicateDialog) {
+        DefaultDialog(
+            title = { Text(stringResource(R.string.duplicates)) },
+            buttons = {
+                TextButton(
+                    onClick = {
+                        showDuplicateDialog = false
+                        onDismiss()
+                        database.transaction {
+                            addSongToPlaylist(
+                                selectedPlaylist!!,
+                                songIds!!.filter {
+                                    !duplicates.contains(it)
+                                }
+                            )
+                        }
+                    }
+                ) {
+                    Text(stringResource(R.string.skip_duplicates))
+                }
+
+                TextButton(
+                    onClick = {
+                        showDuplicateDialog = false
+                        onDismiss()
+                        database.transaction {
+                            addSongToPlaylist(selectedPlaylist!!, songIds!!)
+                        }
+                    }
+                ) {
+                    Text(stringResource(R.string.add_anyway))
+                }
+
+                TextButton(
+                    onClick = {
+                        showDuplicateDialog = false
+                    }
+                ) {
+                    Text(stringResource(android.R.string.cancel))
+                }
+            },
+            onDismiss = {
+                showDuplicateDialog = false
+            }
+        ) {
+            Text(
+                text = if (duplicates.size == 1) {
+                    stringResource(R.string.duplicates_description_single)
+                } else {
+                    stringResource(R.string.duplicates_description_multiple, duplicates.size)
+                },
+                textAlign = TextAlign.Start,
+                modifier = Modifier.align(Alignment.Start)
+            )
+        }
     }
 }
