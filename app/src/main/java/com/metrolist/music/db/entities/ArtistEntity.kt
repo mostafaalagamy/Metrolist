4f3e230b83fc7ea1449b555c996d9ece68e04dc8
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2024-12-29 17:54:07 +0200

    Update ArtistEntity.kt

--- app/src/main/java/com/metrolist/music/db/entities/ArtistEntity.kt
+++ app/src/main/java/com/metrolist/music/db/entities/ArtistEntity.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.Immutable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.apache.commons.lang3.RandomStringUtils
+import com.metrolist.innertube.YouTube
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
 import java.time.LocalDateTime
 
 @Immutable
@@ -12,6 +18,7 @@ data class ArtistEntity(
     @PrimaryKey val id: String,
     val name: String,
     val thumbnailUrl: String? = null,
+    val channelId: String? = null,
     val lastUpdateTime: LocalDateTime = LocalDateTime.now(),
     val bookmarkedAt: LocalDateTime? = null,
 ) {
@@ -21,10 +28,18 @@ data class ArtistEntity(
     val isLocalArtist: Boolean
         get() = id.startsWith("LA")
 
-    fun toggleLike() =
+    fun localToggleLike() = copy(
         copy(
             bookmarkedAt = if (bookmarkedAt != null) null else LocalDateTime.now(),
-        )
+    ).also {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (channelId == null)
+                YouTube.subscribeChannel(YouTube.getChannelId(id), bookmarkedAt == null)
+            else
+                YouTube.subscribeChannel(channelId, bookmarkedAt == null)
+            this.cancel()
+        }
+    }
 
     companion object {
         fun generateArtistId() = "LA" + RandomStringUtils.random(8, true, false)
