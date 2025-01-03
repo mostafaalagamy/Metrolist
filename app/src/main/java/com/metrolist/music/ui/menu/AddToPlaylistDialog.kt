66ccd3e77345013ccc6d417c0bd82a41d855d32a
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-03 20:35:13 +0200

    Update AddToPlaylistDialog.kt

--- app/src/main/java/com/metrolist/music/ui/menu/AddToPlaylistDialog.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/AddToPlaylistDialog.kt
@@ -147,10 +147,41 @@ fun AddToPlaylistDialog(
                         insert(
                             PlaylistEntity(
                                 name = playlistName,
-                                browseId = browseId,
+                                browseId = if (syncedPlaylist) browseId else null,
                                 bookmarkedAt = LocalDateTime.now(),
                                 isEditable = !syncedPlaylist,
+                                isLocal = !syncedPlaylist // && check that all songs are non-local
                             )
+                        )
+                    }
+                }
+            },
+            extraContent = {
+                // synced/unsynced toggle
+                Row(
+                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 40.dp)
+                ) {
+                    Column() {
+                        Text(
+                            text = "Sync Playlist",
+                            style = MaterialTheme.typography.titleLarge,
+                        )
+                        Text(
+                            text = "Note: This allows for syncing with YouTube Music. This is NOT changeable later. You cannot add local songs to synced playlists.",
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.fillMaxWidth(0.7f)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Switch(
+                            enabled = !noSyncing,
+                            checked = syncedPlaylist,
+                            onCheckedChange = {
+                                syncedPlaylist = !syncedPlaylist
+                            },
                         )
                     }
                 }
