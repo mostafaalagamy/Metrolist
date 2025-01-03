902c84e72e6718ca5f423a37a1585e9024780ee1
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-03 22:35:03 +0200

    Update AlbumMenu.kt

--- app/src/main/java/com/metrolist/music/ui/menu/AlbumMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/AlbumMenu.kt
@@ -149,7 +149,16 @@ fun AlbumMenu(
 
     AddToPlaylistDialog(
         isVisible = showChoosePlaylistDialog,
-        onGetSong = { songs.map { it.id } },
+        onGetSong = { playlist ->
+            coroutineScope.launch(Dispatchers.IO) {
+                playlist.playlist.browseId?.let { playlistId ->
+                    album.album.playlistId?.let { addPlaylistId ->
+                        YouTube.addPlaylistToPlaylist(playlistId, addPlaylistId)
+                    }
+                }
+            }
+            songs.map { it.id }
+        },
         onDismiss = {
             showChoosePlaylistDialog = false
         },
