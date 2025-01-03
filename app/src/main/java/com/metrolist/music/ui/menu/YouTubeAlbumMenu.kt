01cfb117e5598b0116a6db9edf0f538d215d4592
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-03 23:19:58 +0200

    Update YouTubeAlbumMenu.kt

--- app/src/main/java/com/metrolist/music/ui/menu/YouTubeAlbumMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/YouTubeAlbumMenu.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -64,6 +65,8 @@ import com.metrolist.music.ui.component.ListItem
 import com.metrolist.music.ui.component.SongListItem
 import com.metrolist.music.ui.component.YouTubeListItem
 import com.metrolist.music.utils.reportException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
 @SuppressLint("MutableCollectionMutableState")
