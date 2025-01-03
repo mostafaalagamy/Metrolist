88b66871839201ee322960af875e60d666b394b7
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-04 00:02:26 +0200

    Update PlaylistMenu.kt

--- app/src/main/java/com/metrolist/music/ui/menu/PlaylistMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/PlaylistMenu.kt
@@ -43,6 +43,7 @@ import com.metrolist.music.LocalPlayerConnection
 import com.metrolist.music.R
 import com.metrolist.music.db.entities.Playlist
 import com.metrolist.music.db.entities.PlaylistSong
+import com.metrolist.music.db.entities.PlaylistSongMap
 import com.metrolist.music.db.entities.Song
 import com.metrolist.music.extensions.toMediaItem
 import com.metrolist.music.models.toMediaMetadata
@@ -264,6 +265,10 @@ fun PlaylistMenu(
                 }
             },
         )
+    }
+
+    var showChoosePlaylistDialog by rememberSaveable {
+        mutableStateOf(false)
     }
 
     AddToPlaylistDialog(
