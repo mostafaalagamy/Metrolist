57a9038dd9f94844138ede00f28a8cde933aa33c
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-09 22:54:55 +0200

    Update SyncUtils.kt

--- app/src/main/java/com/metrolist/music/utils/SyncUtils.kt
+++ app/src/main/java/com/metrolist/music/utils/SyncUtils.kt
@@ -97,6 +97,7 @@ class SyncUtils @Inject constructor(
             val dbPlaylists = database.playlistsByCreateDateAsc().first()
             dbPlaylists
                 .filterNot { it.playlist.browseId in playlistList.map(PlaylistItem::id) }
+                .filterNot { it.playlist.browseId == null }
                 .forEach { database.update(it.playlist.localToggleLike()) }
             playlistList.onEach { playlist ->
                 var playlistEntity = dbPlaylists.find { playlist.id == it.playlist.browseId }?.playlist
