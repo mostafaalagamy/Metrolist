47a45f10eb2e4b127ca357fd53df7c0ba709a57a
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-10 05:24:06 +0200

    Update SelectionSongsMenu.kt

--- app/src/main/java/com/metrolist/music/ui/menu/SelectionSongsMenu.kt
+++ app/src/main/java/com/metrolist/music/ui/menu/SelectionSongsMenu.kt
@@ -209,6 +209,31 @@ fun SelectionSongMenu(
             clearAction()
         }
 
+        if (allInLibrary) {
+            GridMenuItem(
+                icon = R.drawable.library_add_check,
+                title = R.string.remove_from_library,
+            ) {
+                database.query {
+                    songSelection.forEach { song ->
+                        inLibrary(song.id, null)
+                    }
+                }
+            }
+        } else {
+            GridMenuItem(
+                icon = R.drawable.library_add,
+                title = R.string.add_to_library,
+            ) {
+                database.transaction {
+                    songSelection.forEach { song ->
+                        insert(song.toMediaMetadata())
+                        inLibrary(song.id, LocalDateTime.now())
+                    }
+                }
+            }
+        }
+
         GridMenuItem(
             icon = R.drawable.playlist_add,
             title = R.string.add_to_playlist,
