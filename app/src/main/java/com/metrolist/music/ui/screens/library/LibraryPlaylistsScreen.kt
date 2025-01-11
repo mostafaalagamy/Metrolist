9e91ab5cde02b3eeaa48e7b15e39061d8b595074
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-11 16:18:28 +0200

    Update LibraryPlaylistsScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/library/LibraryPlaylistsScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/library/LibraryPlaylistsScreen.kt
@@ -234,7 +234,7 @@ fun LibraryPlaylistsScreen(
             Spacer(Modifier.weight(1f))
 
             Text(
-                text = pluralStringResource(R.plurals.n_playlist, playlists.size, playlists.size)
+                text = pluralStringResource(R.plurals.n_playlist, playlists!!.size, playlists!!.size),
                 style = MaterialTheme.typography.titleSmall,
                 color = MaterialTheme.colorScheme.secondary,
             )
