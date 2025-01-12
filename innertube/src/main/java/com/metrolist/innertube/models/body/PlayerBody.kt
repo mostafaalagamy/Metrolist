13879b3354326edb7999b390f65096092db036da
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-12 18:10:50 +0200

    feat: deobfuscate stream urls with NewPipeExtractor, colse #182, #191, #206, #213

--- innertube/src/main/java/com/metrolist/innertube/models/body/PlayerBody.kt
+++ innertube/src/main/java/com/metrolist/innertube/models/body/PlayerBody.kt
@@ -8,15 +8,17 @@ data class PlayerBody(
     val context: Context,
     val videoId: String,
     val playlistId: String?,
-    val cpn: String? = "wzf9Y0nqz6AUe2Vr", // need some random cpn to get same algorithm for sig
-    val playbackContext: PlaybackContext? = PlaybackContext(ContentPlaybackContext(20052L)),
+    val playbackContext: PlaybackContext? = null,
+    val contentCheckOk: Boolean = true,
+    val racyCheckOk: Boolean = true,
 ) {
     @Serializable
     data class PlaybackContext(
-        val contentPlaybackContext: ContentPlaybackContext?,
-    )
-    @Serializable
-    data class ContentPlaybackContext(
-        val signatureTimestamp: Long?,
-    )
+        val contentPlaybackContext: ContentPlaybackContext
+    ) {
+        @Serializable
+        data class ContentPlaybackContext(
+            val signatureTimestamp: Int
+        )
+    }
 }
