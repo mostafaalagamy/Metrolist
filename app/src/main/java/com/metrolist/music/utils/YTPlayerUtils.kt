ffefe44eb052df12c4696628dc6028268a801928
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-12 15:58:17 +0200

    Update YTPlayerUtils.kt

--- app/src/main/java/com/metrolist/music/utils/YTPlayerUtils.kt
+++ app/src/main/java/com/metrolist/music/utils/YTPlayerUtils.kt
@@ -156,11 +156,13 @@ object YTPlayerUtils {
             playerResponse.streamingData?.adaptiveFormats
                 ?.filter { it.isAudio }
                 ?.maxByOrNull {
-                    it.bitrate * when (audioQuality) {
-                        AudioQuality.AUTO -> if (connectivityManager.isActiveNetworkMetered) -1 else 1
-                        AudioQuality.HIGH -> 1
-                        AudioQuality.LOW -> -1
-                    } + (if (it.mimeType.startsWith("audio/webm")) 10240 else 0) // prefer opus stream
+                    it.bitrate * 
+                        when (audioQuality) {
+                            AudioQuality.AUTO -> if (connectivityManager.isActiveNetworkMetered) -1 else 1 - 5
+                            AudioQuality.MAX -> 5
+                            AudioQuality.HIGH -> 1
+                            AudioQuality.LOW -> -1
+                        } + (if (it.mimeType.startsWith("audio/webm")) 10240 else 0) // prefer opus stream
                 }
         }
     /**
