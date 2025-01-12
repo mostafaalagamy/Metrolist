77b826b8fb69c500fb065b46ced74d0239ec379d
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-12 17:32:04 +0200

    Update YTPlayerUtils.kt

--- app/src/main/java/com/metrolist/music/utils/YTPlayerUtils.kt
+++ app/src/main/java/com/metrolist/music/utils/YTPlayerUtils.kt
@@ -158,7 +158,8 @@ object YTPlayerUtils {
                 ?.maxByOrNull {
                     it.bitrate * 
                         when (audioQuality) {
-                            AudioQuality.AUTO -> if (connectivityManager.isActiveNetworkMetered) -1 else 1
+                            AudioQuality.AUTO -> if (connectivityManager.isActiveNetworkMetered) -1 else 1 - 5
+                            AudioQuality.MAX -> 5
                             AudioQuality.HIGH -> 1
                             AudioQuality.LOW -> -1
                         } + (if (it.mimeType.startsWith("audio/webm")) 10240 else 0) // prefer opus stream
