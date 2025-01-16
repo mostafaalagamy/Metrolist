cbf39106f4d361906db0c9c20d27c4552e17b4c9
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-16 23:12:44 +0200

    Update ArtistScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/artist/ArtistScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/artist/ArtistScreen.kt
@@ -61,7 +61,6 @@ import com.metrolist.innertube.models.AlbumItem
 import com.metrolist.innertube.models.ArtistItem
 import com.metrolist.innertube.models.PlaylistItem
 import com.metrolist.innertube.models.SongItem
-import com.metrolist.innertube.models.WatchEndpoint
 import com.metrolist.music.LocalDatabase
 import com.metrolist.music.LocalPlayerAwareWindowInsets
 import com.metrolist.music.LocalPlayerConnection
@@ -259,12 +258,7 @@ fun ArtistScreen(
                                                 playerConnection.player.togglePlayPause()
                                             } else {
                                                 playerConnection.playQueue(
-                                                    YouTubeQueue(
-                                                        WatchEndpoint(
-                                                            videoId = song.id,
-                                                        ),
-                                                        song.toMediaMetadata(),
-                                                    ),
+                                                    YouTubeQueue.radio(item.toMediaMetadata()),
                                                 )
                                             }
                                         },
