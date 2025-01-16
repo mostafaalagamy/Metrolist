78b77bbf1d5638c2472785a8d2c014198e239046
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-16 15:48:13 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
 }
 
 rootProject.name = "Metrolist"
