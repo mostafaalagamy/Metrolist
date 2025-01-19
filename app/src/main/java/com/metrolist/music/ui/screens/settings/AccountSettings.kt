9e63b6c2e0b598ca0d2e579bf578b13ab4da2ae6
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-19 14:56:15 +0200

    fix: improve advanced login option in account screen

--- app/src/main/java/com/metrolist/music/ui/screens/settings/AccountSettings.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/settings/AccountSettings.kt
@@ -139,25 +139,27 @@ fun AccountSettings(
                 )
             }
 
-            if (isLoggedIn) {
-                PreferenceEntry(
-                    title = {
+            PreferenceEntry(
+                title = {
+                    if (!isLoggedIn) {
+                        Text(stringResource(R.string.advanced_login))
+                    } else {
                         if (showToken) {
                             Text(stringResource(R.string.token_shown))
                         } else {
                             Text(stringResource(R.string.token_hidden))
                         }
-                    },
-                    icon = { Icon(painterResource(R.drawable.token), null) },
-                    onClick = {
-                        if (showToken == false) {
-                            showToken = true
-                        } else {
-                            showTokenEditor = true
-                        }
-                    },
-                )
-            }
+                    }
+                },
+                icon = { Icon(painterResource(R.drawable.token), null) },
+                onClick = {
+                    if (showToken == false) {
+                        showToken = true
+                    } else {
+                        showTokenEditor = true
+                    }
+                },
+            )
 
             if (isLoggedIn) {
                 SwitchPreference(
