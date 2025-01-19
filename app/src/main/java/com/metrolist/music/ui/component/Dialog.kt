e265a3b2c1486d6b57320d5c6999dfee65003f3c
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-01-19 04:03:41 +0200

    Update Dialog.kt

--- app/src/main/java/com/metrolist/music/ui/component/Dialog.kt
+++ app/src/main/java/com/metrolist/music/ui/component/Dialog.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.metrolist.music.R
 import kotlinx.coroutines.delay
 
 @Composable
