46a56ed701b9ba3a586a8a06d8653f79111086a1
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-03-25 23:33:46 +0200

    Update App.kt

--- app/src/main/java/com/metrolist/music/App.kt
+++ app/src/main/java/com/metrolist/music/App.kt
@@ -66,7 +66,7 @@ class App : Application(), ImageLoaderFactory {
             }
         }
 
-        if (dataStore[UseLoginForBrowse] != false) {
+        if (dataStore[UseLoginForBrowse] == true) {
             YouTube.useLoginForBrowse = true
         }
 
