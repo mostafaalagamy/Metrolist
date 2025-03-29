2276cf226f4c594f92c39ffac3812ada44f525a0
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-03-29 21:22:10 +0200

    Update HomeScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/HomeScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -158,6 +159,16 @@ fun HomeScreen(
             lazylistState.animateScrollToItem(0)
             backStackEntry?.savedStateHandle?.set("scrollToTop", false)
         }
+    }
+
+    LaunchedEffect(Unit) {
+         snapshotFlow { lazylistState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
+             .collect { lastVisibleIndex ->
+                 val len = lazylistState.layoutInfo.totalItemsCount
+                 if (lastVisibleIndex != null && lastVisibleIndex >= len - 3) {
+                     viewModel.loadMoreYouTubeItems(homePage?.continuation)
+                 }
+             }
     }
 
     val localGridItem: @Composable (LocalItem) -> Unit = {
