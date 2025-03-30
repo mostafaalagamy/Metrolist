a8d7a3dff04570236418baad0419e7b80db5a476
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-03-30 04:39:47 +0200

    Create NewReleaseViewModel.kt

---
+++ app/src/main/java/com/metrolist/music/viewmodels/NewReleaseViewModel.kt
@@ -0,0 +1,68 @@
+package com.metrolist.music.viewmodels
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.metrolist.innertube.YouTube
+import com.metrolist.innertube.models.AlbumItem
+import com.metrolist.innertube.models.filterExplicit
+import com.metrolist.music.constants.HideExplicitKey
+import com.metrolist.music.db.MusicDatabase
+import com.metrolist.music.utils.dataStore
+import com.metrolist.music.utils.get
+import com.metrolist.music.utils.reportException
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewReleaseViewModel
+@Inject
+constructor(
+    @ApplicationContext val context: Context,
+    database: MusicDatabase,
+) : ViewModel() {
+    private val _newReleaseAlbums = MutableStateFlow<List<AlbumItem>>(emptyList())
+    val newReleaseAlbums = _newReleaseAlbums.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            YouTube
+                .newReleaseAlbums()
+                .onSuccess { albums ->
+                    val artists: MutableMap<Int, String> = mutableMapOf()
+                    val favouriteArtists: MutableMap<Int, String> = mutableMapOf()
+                    database.allArtistsByPlayTime().first().let { list ->
+                        var favIndex = 0
+                        for ((artistsIndex, artist) in list.withIndex()) {
+                            artists[artistsIndex] = artist.id
+                            if (artist.artist.bookmarkedAt != null) {
+                                favouriteArtists[favIndex] = artist.id
+                                favIndex++
+                            }
+                        }
+                    }
+                    _newReleaseAlbums.value =
+                        albums
+                            .sortedBy { album ->
+                                val artistIds = album.artists.orEmpty().mapNotNull { it.id }
+                                val firstArtistKey =
+                                    artistIds.firstNotNullOfOrNull { artistId ->
+                                        if (artistId in favouriteArtists.values) {
+                                            favouriteArtists.entries.firstOrNull { it.value == artistId }?.key
+                                        } else {
+                                            artists.entries.firstOrNull { it.value == artistId }?.key
+                                        }
+                                    } ?: Int.MAX_VALUE
+                                firstArtistKey
+                            }.filterExplicit(context.dataStore.get(HideExplicitKey, false))
+                }.onFailure {
+                    reportException(it)
+                }
+        }
+    }
+}
