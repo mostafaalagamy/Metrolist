a3ec6c833a0239535aca912409990d0f638f1ae2
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-02 03:39:37 +0200

    Update ChartsScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/ChartsScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/ChartsScreen.kt
@@ -34,7 +34,7 @@ import com.metrolist.music.models.toMediaMetadata
 import com.metrolist.music.playback.queues.YouTubeQueue
 import com.metrolist.music.ui.component.LocalMenuState
 import com.metrolist.music.ui.component.NavigationTitle
-import com.metrolist.music.ui.component.SongListItem
+import com.metrolist.music.ui.component.YouTubeListItem
 import com.metrolist.music.ui.component.shimmer.GridItemPlaceHolder
 import com.metrolist.music.ui.component.shimmer.ShimmerHost
 import com.metrolist.music.ui.component.shimmer.TextPlaceholder
