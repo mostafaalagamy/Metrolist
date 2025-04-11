010a547aa2014e8e744f17e974983acd76d7740f
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-12 01:11:03 +0200

    Update LibrarySongsScreen.kt

--- app/src/main/java/com/metrolist/music/ui/screens/library/LibrarySongsScreen.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/library/LibrarySongsScreen.kt
@@ -64,6 +64,8 @@ import com.metrolist.music.ui.utils.ItemWrapper
 import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
 import com.metrolist.music.viewmodels.LibrarySongsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -89,14 +91,10 @@ fun LibrarySongsScreen(
 
     val songs by viewModel.allSongs.collectAsState()
 
-    var filter by rememberEnumPreference(SongFilterKey, SongFilter.LIKED)
-
     LaunchedEffect(Unit) {
         if (ytmSync) {
-            when (filter) {
-                SongFilter.LIKED -> viewModel.syncLikedSongs()
-                SongFilter.LIBRARY -> viewModel.syncLibrarySongs()
-                else -> return@LaunchedEffect
+                withContext(Dispatchers.IO) {
+                    viewModel.syncLikedSongs()
             }
         }
     }
