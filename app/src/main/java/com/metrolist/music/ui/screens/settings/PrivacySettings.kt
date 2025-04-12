d15c9a108e75c79cf3adee9a593577663597fa50
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-12 19:00:16 +0200

    Update PrivacySettings.kt

--- app/src/main/java/com/metrolist/music/ui/screens/settings/PrivacySettings.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/settings/PrivacySettings.kt
@@ -30,6 +30,7 @@ import com.metrolist.music.LocalPlayerAwareWindowInsets
 import com.metrolist.music.R
 import com.metrolist.music.constants.DisableScreenshotKey
 import com.metrolist.music.constants.PauseListenHistoryKey
+import com.metrolist.music.constants.PauseRemoteListenHistoryKey
 import com.metrolist.music.constants.PauseSearchHistoryKey
 import com.metrolist.music.ui.component.DefaultDialog
 import com.metrolist.music.ui.component.IconButton
@@ -53,6 +54,10 @@ fun PrivacySettings(
     val (pauseSearchHistory, onPauseSearchHistoryChange) = rememberPreference(
         key = PauseSearchHistoryKey,
         defaultValue = false
+    )
+    val (pauseRemoteListenHistory, onPauseRemoteListenHistoryChange) = rememberPreference(
+        key = PauseRemoteListenHistoryKey,
+        defaultValue = true
     )
     val (disableScreenshot, onDisableScreenshotChange) = rememberPreference(
         key = DisableScreenshotKey,
@@ -151,6 +156,13 @@ fun PrivacySettings(
             icon = { Icon(painterResource(R.drawable.history), null) },
             checked = pauseListenHistory,
             onCheckedChange = onPauseListenHistoryChange,
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.pause_listen_history)) },
+            icon = { Icon(painterResource(R.drawable.history), null) },
+            checked = pauseRemoteListenHistory,
+            onCheckedChange = onPauseRemoteListenHistoryChange,
+            isEnabled = pauseListenHistory
         )
         PreferenceEntry(
             title = { Text(stringResource(R.string.clear_listen_history)) },
