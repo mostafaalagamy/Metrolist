7adfcdd5d99dfa570ec2a6e48547f728e3d7c9fd
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-13 19:28:45 +0200

    Update Items.kt

--- app/src/main/java/com/metrolist/music/ui/component/Items.kt
+++ app/src/main/java/com/metrolist/music/ui/component/Items.kt
@@ -1395,6 +1395,7 @@ fun PlaylistGridItem(
             when (playlist.playlist.name) {
                 stringResource(R.string.liked) -> R.drawable.favorite_border
                 stringResource(R.string.offline) -> R.drawable.offline
+                stringResource(R.string.cached_songs) -> R.drawable.cached
                 else -> {
                     if (autoPlaylist) {
                         R.drawable.trending_up
