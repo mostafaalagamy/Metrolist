a14d3b80f3e5ed90041dc3b66710d7fa01be036f
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-13 05:15:41 +0200

    Create OfflineViewModel.kt

---
+++ app/src/main/java/com/metrolist/music/viewmodels/OfflineViewModel.kt
@@ -0,0 +1,52 @@
+package com.metrolist.music.viewmodels
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.metrolist.music.constants.SongSortDescendingKey
+import com.metrolist.music.constants.SongSortType
+import com.metrolist.music.constants.SongSortTypeKey
+import com.metrolist.music.db.MusicDatabase
+import com.metrolist.music.extensions.reversed
+import com.metrolist.music.extensions.toEnum
+import com.metrolist.music.utils.dataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltViewModel
+class OfflineViewModel @Inject constructor(
+    @ApplicationContext context: Context,
+    database: MusicDatabase,
+) : ViewModel() {
+    
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val offlineSongs = context.dataStore.data
+        .map {
+            it[SongSortTypeKey].toEnum(SongSortType.CREATE_DATE) to (it[SongSortDescendingKey] ?: true)
+        }
+        .distinctUntilChanged()
+        .flatMapLatest { (sortType, descending) ->
+            database.events().map { events ->
+                events.map { it.song }
+                    .distinctBy { it.id }
+                    .sortedWith(
+                        when (sortType) {
+                            SongSortType.CREATE_DATE -> compareBy { it.song.createDate }
+                            SongSortType.NAME -> compareBy { it.song.title }
+                            SongSortType.ARTIST -> compareBy { song -> 
+                                song.artists.joinToString(separator = "") { it.name }
+                            }
+                            SongSortType.PLAY_TIME -> compareBy { it.song.totalPlayTime }
+                        }.reversed(descending)
+            }
+        }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+}
