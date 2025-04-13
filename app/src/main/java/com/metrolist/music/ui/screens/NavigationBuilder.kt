94127f278db9c7d0fcd878a606d4347b051b34ca
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-13 20:44:10 +0200

    Update NavigationBuilder.kt

--- app/src/main/java/com/metrolist/music/ui/screens/NavigationBuilder.kt
+++ app/src/main/java/com/metrolist/music/ui/screens/NavigationBuilder.kt
@@ -52,7 +52,7 @@ import com.metrolist.music.ui.screens.playlist.AutoPlaylistScreen
 import com.metrolist.music.ui.screens.playlist.LocalPlaylistScreen
 import com.metrolist.music.ui.screens.playlist.OnlinePlaylistScreen
 import com.metrolist.music.ui.screens.playlist.TopPlaylistScreen
-import com.metrolist.music.ui.screens.playlist.CacheSongsScreen
+import com.metrolist.music.ui.screens.playlist.CachePlaylistScreen
 import com.metrolist.music.ui.screens.search.OnlineSearchResult
 import com.metrolist.music.ui.screens.settings.AboutScreen
 import com.metrolist.music.ui.screens.settings.AccountSettings
