be1ae6cae9642c8a2f38a4439888ab8303a0784e
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-16 19:14:20 +0200

    CachePlaylistViewModel

--- app/src/main/java/com/metrolist/music/viewmodels/CachePlaylistViewModel.kt
+++ app/src/main/java/com/metrolist/music/viewmodels/CachePlaylistViewModel.kt
@@ -3,16 +3,20 @@ package com.metrolist.music.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.metrolist.music.db.MusicDatabase
+import com.metrolist.music.db.entities.FormatEntity
 import com.metrolist.music.db.entities.Song
+import com.metrolist.music.di.PlayerCache
+import com.metrolist.music.di.DownloadCache
+import com.metrolist.music.utils.YTPlayerUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import javax.inject.Inject
-import com.metrolist.music.di.PlayerCache
-import com.metrolist.music.di.DownloadCache
-import androidx.media3.datasource.cache.SimpleCache
+import kotlinx.coroutines.runBlocking
 import java.time.LocalDateTime
+import javax.inject.Inject
+import androidx.media3.datasource.cache.SimpleCache
 
 @HiltViewModel
 class CachePlaylistViewModel @Inject constructor(
@@ -25,7 +29,7 @@ class CachePlaylistViewModel @Inject constructor(
     val cachedSongs: StateFlow<List<Song>> = _cachedSongs
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             while (true) {
                 val cachedIds = playerCache.keys.mapNotNull { it?.toString() }.toSet()
                 val downloadedIds = downloadCache.keys.mapNotNull { it?.toString() }.toSet()
@@ -37,17 +41,42 @@ class CachePlaylistViewModel @Inject constructor(
                     emptyList()
                 }
 
-                val completeSongs = songs.filter {
-                    val contentLength = it.format?.contentLength
-                    contentLength != null && playerCache.isCached(it.song.id, 0, contentLength)
-                }
+                val completeSongs = mutableListOf<Song>()
 
-                if (completeSongs.isNotEmpty()) {
-                    database.query {
-                        completeSongs.forEach {
-                            if (it.song.dateDownload == null) {
-                                update(it.song.copy(dateDownload = LocalDateTime.now()))
+                database.query {
+                    songs.forEach { song ->
+                        val songId = song.song.id
+
+                        if (song.format == null) {
+                            runCatching {
+                                val result = YTPlayerUtils.playerResponseForPlayback(songId)
+                                val format = result.getOrThrow().format
+
+                                upsert(
+                                    FormatEntity(
+                                        id = songId,
+                                        itag = format.itag,
+                                        mimeType = format.mimeType.split(";")[0],
+                                        codecs = format.mimeType.split("codecs=")[1].removeSurrounding("\""),
+                                        bitrate = format.bitrate,
+                                        sampleRate = format.audioSampleRate,
+                                        contentLength = format.contentLength!!,
+                                        loudnessDb = result.getOrThrow().audioConfig?.loudnessDb,
+                                        playbackUrl = result.getOrThrow().playbackTracking?.videostatsPlaybackUrl?.baseUrl
+                                    )
+                                )
                             }
+                        }
+
+                        val updated = database.getSongById(songId)
+                        val contentLength = updated?.format?.contentLength
+
+                        if (contentLength != null && playerCache.isCached(songId, 0, contentLength)) {
+                            if (updated.song.dateDownload == null) {
+                                update(updated.song.copy(dateDownload = LocalDateTime.now()))
+                            }
+
+                            completeSongs.add(updated)
                         }
                     }
                 }
