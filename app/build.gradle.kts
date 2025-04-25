d88f83a736786067521165435b465f58567bb1b7
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-25 06:24:22 +0300

    fix: refactor Gradle signing to support dynamic keystore selection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,11 @@ android {
         debug {
             applicationIdSuffix = ".debug"
             isDebuggable = true
-            signingConfig = signingConfigs.getByName("persistentDebug")
+            signingConfig = if (System.getenv("GITHUB_EVENT_NAME") == "pull_request") {
+                signingConfigs.getByName("debug")
+            } else {
+                signingConfigs.getByName("persistentDebug")
+            }
         }
     }
 
