0913fb42afd8b70b277558322464c82a6b88da91
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-26 21:51:28 +0300

    PlayerResponse.kt

--- innertube/src/main/java/com/metrolist/innertube/models/response/PlayerResponse.kt
+++ innertube/src/main/java/com/metrolist/innertube/models/response/PlayerResponse.kt
@@ -2,7 +2,6 @@ package com.metrolist.innertube.models.response
 
 import com.metrolist.innertube.models.ResponseContext
 import com.metrolist.innertube.models.Thumbnails
-import com.metrolist.innertube.utils.createUrl
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -65,8 +64,6 @@ data class PlayerResponse(
         ) {
             val isAudio: Boolean
                 get() = width == null
-
-            fun findUrl() = url?.let { createUrl(url = it) } ?: signatureCipher?.let { createUrl(cipher = it) }!!
         }
     }
 
