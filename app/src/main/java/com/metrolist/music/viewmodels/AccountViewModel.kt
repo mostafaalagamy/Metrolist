64438b66824d347d4fbd69a1a992bada0a562e94
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-26 21:49:08 +0300

    AccountViewModel.kt

--- app/src/main/java/com/metrolist/music/viewmodels/AccountViewModel.kt
+++ app/src/main/java/com/metrolist/music/viewmodels/AccountViewModel.kt
@@ -6,7 +6,7 @@ import com.metrolist.innertube.YouTube
 import com.metrolist.innertube.models.AlbumItem
 import com.metrolist.innertube.models.ArtistItem
 import com.metrolist.innertube.models.PlaylistItem
-import com.metrolist.innertube.utils.completedLibraryPage
+import com.metrolist.innertube.utils.completed
 import com.metrolist.music.utils.reportException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,18 +21,18 @@ class AccountViewModel @Inject constructor() : ViewModel() {
 
     init {
         viewModelScope.launch {
-            YouTube.library("FEmusic_liked_playlists").completedLibraryPage().onSuccess {
+            YouTube.library("FEmusic_liked_playlists").completed().onSuccess {
                 playlists.value = it.items.filterIsInstance<PlaylistItem>()
                     .filterNot { it.id == "SE" }
             }.onFailure {
                 reportException(it)
             }
-            YouTube.library("FEmusic_liked_albums").completedLibraryPage().onSuccess {
+            YouTube.library("FEmusic_liked_albums").completed().onSuccess {
                 albums.value = it.items.filterIsInstance<AlbumItem>()
             }.onFailure {
                 reportException(it)
             }
-            YouTube.library("FEmusic_library_corpus_artists").completedLibraryPage().onSuccess {
+            YouTube.library("FEmusic_library_corpus_artists").completed().onSuccess {
                 artists.value = it.items.filterIsInstance<ArtistItem>()
             }.onFailure {
                 reportException(it)
