f9dbc176663132a948acfd49dceda3fbb61a15d0
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-30 17:19:30 +0300

    MainActivity.kt

--- app/src/main/kotlin/com/metrolist/music/MainActivity.kt
+++ app/src/main/kotlin/com/metrolist/music/MainActivity.kt
@@ -278,8 +278,6 @@ class MainActivity : ComponentActivity() {
 
             val enableDynamicTheme by rememberPreference(DynamicThemeKey, defaultValue = true)
             val darkTheme by rememberEnumPreference(DarkModeKey, defaultValue = DarkMode.AUTO)
-            val pureBlackEnabled by rememberPreference(PureBlackKey, defaultValue = false)
-            val pureBlack = pureBlackEnabled && useDarkTheme
             val isSystemInDarkTheme = isSystemInDarkTheme()
             val useDarkTheme =
                 remember(darkTheme, isSystemInDarkTheme) {
@@ -288,6 +286,9 @@ class MainActivity : ComponentActivity() {
             LaunchedEffect(useDarkTheme) {
                 setSystemBarAppearance(useDarkTheme)
             }
+            val pureBlackEnabled by rememberPreference(PureBlackKey, defaultValue = false)
+            val pureBlack = pureBlackEnabled && useDarkTheme
+
             var themeColor by rememberSaveable(stateSaver = ColorSaver) {
                 mutableStateOf(DefaultThemeColor)
             }
