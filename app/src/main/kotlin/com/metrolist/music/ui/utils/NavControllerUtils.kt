4f60d980d27976c6f5a2c6238721ae864c5e9d0e
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-04-30 23:07:53 +0300

    NavControllerUtils.kt

--- app/src/main/kotlin/com/metrolist/music/ui/utils/NavControllerUtils.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/utils/NavControllerUtils.kt
@@ -3,9 +3,6 @@ package com.metrolist.music.ui.utils
 import androidx.navigation.NavController
 import com.metrolist.music.ui.screens.Screens
 
-val NavController.canNavigateUp: Boolean
-    get() = currentBackStackEntry?.destination?.parent?.route != null
-
 fun NavController.backToMain() {
     val mainRoutes = Screens.MainScreens.map { it.route }
 
