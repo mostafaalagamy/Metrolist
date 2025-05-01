37d9da30d18366dcde78cfe1ef23c75f2c219915
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-05-01 03:09:58 +0300

    Items.kt

--- app/src/main/kotlin/com/metrolist/music/ui/component/Items.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/Items.kt
@@ -788,10 +788,7 @@ fun YouTubeListItem(
         ListItem(
             title = item.title,
             subtitle = when (item) {
-                is SongItem -> joinByBullet(
-                    item.artistName ?: item.artists.joinToString { it.name },
-                    makeTimeString(item.duration?.times(1000L))
-                )
+                is SongItem -> joinByBullet(item.artists.joinToString { it.name }, makeTimeString(item.duration?.times(1000L)))
                 is AlbumItem -> joinByBullet(item.artists?.joinToString { it.name }, item.year?.toString())
                 is ArtistItem -> null
                 is PlaylistItem -> joinByBullet(item.author?.name, item.songCountText)
@@ -867,10 +864,7 @@ fun YouTubeGridItem(
     },
     subtitle = {
         val subtitle = when (item) {
-            is SongItem -> joinByBullet(
-                item.artistName ?: item.artists.joinToString { it.name },
-                makeTimeString(item.duration?.times(1000L))
-            )
+            is SongItem -> joinByBullet(item.artists.joinToString { it.name }, makeTimeString(item.duration?.times(1000L)))
             is AlbumItem -> joinByBullet(item.artists?.joinToString { it.name }, item.year?.toString())
             is ArtistItem -> null
             is PlaylistItem -> joinByBullet(item.author?.name, item.songCountText)
