b13f0f9bf893d0b0a8c83d901c26f33e169c7327
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-14 08:25:26 +0300

    progress: partial Material 3 Expressive migration

--- app/src/main/kotlin/com/metrolist/music/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/theme/Theme.kt
@@ -4,6 +4,8 @@ import android.graphics.Bitmap
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -29,6 +31,7 @@ import com.metrolist.music.ui.theme.AppTypography
 
 val DefaultThemeColor = Color(0xFFED5564)
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun MetrolistTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
