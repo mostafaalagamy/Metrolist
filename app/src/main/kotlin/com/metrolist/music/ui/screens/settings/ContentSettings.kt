ea59ff699163bb8d960d6f8c5ad1ac335454015f
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 07:05:01 +0300

    Update ContentSettings.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/ContentSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/ContentSettings.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -41,7 +41,7 @@ import com.metrolist.music.utils.rememberPreference
 import java.net.Proxy
 import java.util.Locale
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun ContentSettings(
     navController: NavController,
