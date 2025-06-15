789a3593768f9b7352b51b520f0b1d7b4bfb1ee2
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 07:17:14 +0300

    Update PlayerSettings.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -58,7 +58,7 @@ import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
 import kotlin.math.roundToInt
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun PlayerSettings(
     navController: NavController,
