691490dc11684834edc0e21b9131d7adfe36a3ba
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 07:16:38 +0300

    Update Dialog.kt

--- app/src/main/kotlin/com/metrolist/music/ui/component/Dialog.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/Dialog.kt
@@ -18,7 +18,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -121,7 +121,7 @@ fun DefaultDialog(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun ActionPromptDialog(
     title: String? = null,
