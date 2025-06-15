7d4f3f4782fe68137b5fdc86055a713729ecc20a
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 07:29:08 +0300

    Update NewReleaseScreen.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/NewReleaseScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/NewReleaseScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -36,7 +36,7 @@ import com.metrolist.music.ui.menu.YouTubeAlbumMenu
 import com.metrolist.music.ui.utils.backToMain
 import com.metrolist.music.viewmodels.NewReleaseViewModel
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun NewReleaseScreen(
     navController: NavController,
