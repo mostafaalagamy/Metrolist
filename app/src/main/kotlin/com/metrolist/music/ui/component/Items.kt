18f3644dea86462cd8f3c7efb8c7920dc7a6265f
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 07:08:44 +0300

    Update Items.kt

--- app/src/main/kotlin/com/metrolist/music/ui/component/Items.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/Items.kt
@@ -37,7 +37,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -754,7 +754,7 @@ fun MediaMetadataListItem(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun YouTubeListItem(
     item: YTItem,
