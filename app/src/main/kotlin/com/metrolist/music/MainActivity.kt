7fb2ca3ad52d00c41c9805cd12fbde2b6b17ec03
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 06:56:35 +0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/metrolist/music/MainActivity.kt
+++ app/src/main/kotlin/com/metrolist/music/MainActivity.kt
@@ -49,7 +49,7 @@ import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
@@ -259,7 +259,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3ExpressiveApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
