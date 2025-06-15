f27823f55aa92be8adb29eb910d13870c193e6ed
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-15 06:58:49 +0300

    Update LoginScreen.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/LoginScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/LoginScreen.kt
@@ -8,7 +8,7 @@ import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -38,7 +38,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 @SuppressLint("SetJavaScriptEnabled")
-@OptIn(ExperimentalMaterial3Api::class, DelicateCoroutinesApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, DelicateCoroutinesApi::class)
 @Composable
 fun LoginScreen(
     navController: NavController,
