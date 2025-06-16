2ad8044af41231e2b1960a2d929881b560ebdd18
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-16 03:14:37 +0300

    Create ArtistAlbumsViewModel.kt

---
+++ app/src/main/kotlin/com/metrolist/music/viewmodels/ArtistAlbumsViewModel.kt
@@ -0,0 +1,23 @@
+package com.metrolist.music.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.metrolist.music.db.MusicDatabase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class ArtistAlbumsViewModel @Inject constructor(
+    database: MusicDatabase,
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val artistId = savedStateHandle.get<String>("artistId")!!
+    val artist = database.artist(artistId)
+        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+
+    val albums = database.artistAlbumsPreview(artistId)
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+}
