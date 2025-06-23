976ddcd3046cb0c31456d1d8c2762a2bdf1ace77
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-23 20:56:40 +0300

    feat: start radio from playing song in android auto, close #687
    Co-authored-by: Michael Zh <mikooomich@users.noreply.github.com>

--- app/src/main/kotlin/com/metrolist/music/constants/MediaSessionConstants.kt
+++ app/src/main/kotlin/com/metrolist/music/constants/MediaSessionConstants.kt
@@ -5,11 +5,13 @@ import androidx.media3.session.SessionCommand
 
 object MediaSessionConstants {
     const val ACTION_TOGGLE_LIBRARY = "TOGGLE_LIBRARY"
+    const val ACTION_TOGGLE_START_RADIO = "TOGGLE_START_RADIO"
     const val ACTION_TOGGLE_LIKE = "TOGGLE_LIKE"
     const val ACTION_TOGGLE_SHUFFLE = "TOGGLE_SHUFFLE"
     const val ACTION_TOGGLE_REPEAT_MODE = "TOGGLE_REPEAT_MODE"
     val CommandToggleLibrary = SessionCommand(ACTION_TOGGLE_LIBRARY, Bundle.EMPTY)
     val CommandToggleLike = SessionCommand(ACTION_TOGGLE_LIKE, Bundle.EMPTY)
+    val CommandToggleStartRadio = SessionCommand(ACTION_TOGGLE_START_RADIO, Bundle.EMPTY)
     val CommandToggleShuffle = SessionCommand(ACTION_TOGGLE_SHUFFLE, Bundle.EMPTY)
     val CommandToggleRepeatMode = SessionCommand(ACTION_TOGGLE_REPEAT_MODE, Bundle.EMPTY)
 }
