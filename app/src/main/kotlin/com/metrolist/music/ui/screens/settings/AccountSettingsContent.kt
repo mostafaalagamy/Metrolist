4e739bef328ecefcf6700764e79ebe4f28a19757
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-30 04:19:45 +0300

    Update AccountSettingsContent.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/AccountSettingsContent.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/AccountSettingsContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
