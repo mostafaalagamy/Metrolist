162943fdf3da3ab9feed731fa29a1e4ef5555aa7
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-07-03 00:55:46 +0300

    chore: remove Type-safe Project Accessors
    Improve Gradle sync and CI performance
    
    - Disabled `TYPESAFE_PROJECT_ACCESSORS` feature preview in settings.gradle.kts
    - Replaced `projects.xxx` references with `project(":xxx")` in dependencies
    - This improves IDE indexing speed and stability, and reduces build time

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 @file:Suppress("UnstableApiUsage")
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
