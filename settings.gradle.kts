c0f06f03e0c70bdb42c5d2b4ed2148a433949e3d
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-07-03 00:38:08 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 @file:Suppress("UnstableApiUsage")
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
