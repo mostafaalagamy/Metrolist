a2fdf988285a57e2d2241ef555df8c96ebb778a8
Damian Sobczak <sobczakd2017@gmail.com>
2025-07-04 16:07:48 +0200

    fix: Refactors artist extraction for better reliability (#905)
    Improves artist extraction on the Library and Related pages.
    
    The previous logic was fragile and relied on specific index positions of runs within the subtitle,
    which could easily break if the response structure changed.
    
    Now, on the Library page, artist extraction is disabled as it unreliable. On the related page,
    it now searches for an "ARTIST" icon to extract the artist ID.

--- innertube/src/main/kotlin/com/metrolist/innertube/pages/RelatedPage.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/pages/RelatedPage.kt
@@ -69,12 +69,12 @@ data class RelatedPage(
                             renderer.title.runs
                                 ?.firstOrNull()
                                 ?.text ?: return null,
-                        artists = renderer.subtitle?.runs?.oddElements()?.drop(1)?.map {
-                            Artist(
-                                name = it.text,
-                                id = it.navigationEndpoint?.browseEndpoint?.browseId
-                            )
-                        },
+                        artists = listOfNotNull(Artist(
+                                name = "",
+                                id = renderer.menu?.menuRenderer?.items?.find {
+                                    it.menuNavigationItemRenderer?.icon?.iconType == "ARTIST"
+                                }?.menuNavigationItemRenderer?.navigationEndpoint?.browseEndpoint?.browseId,
+                            )),
                         year =
                             renderer.subtitle
                                 ?.runs
@@ -97,11 +97,8 @@ data class RelatedPage(
                             renderer.title.runs
                                 ?.firstOrNull()
                                 ?.text ?: return null,
-                        author = Artist(
-                            name = renderer.subtitle?.runs?.lastOrNull()?.text ?: return null,
-                            id = null
-                        ),
-                        songCountText = renderer.subtitle.runs.getOrNull(4)?.text,
+                        author = null,
+                        songCountText = renderer.subtitle?.runs?.lastOrNull()?.text,
                         thumbnail = renderer.thumbnailRenderer.musicThumbnailRenderer?.getThumbnailUrl() ?: return null,
                         playEndpoint =
                             renderer.thumbnailOverlay
