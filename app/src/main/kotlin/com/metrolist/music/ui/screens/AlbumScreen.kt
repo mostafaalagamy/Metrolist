8192e1a3cf37018ca5901a37fa29b1817687aebb
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-07-16 03:53:43 +0300

    Update AlbumScreen.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/AlbumScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/AlbumScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
