3b56d9ae8d03f352ce79e401b1b1d10b01363878
mostafaalagamy <mostafaalagamy@email.com>
2025-08-09 01:23:29 +0000

    Use PipePipe Extractor to resolve playback issues
    This change switches from NewPipe Extractor to PipePipe Extractor to resolve
    common playback issues including:
    - Playback Error 403
    - Source error 2004
    - Playback stops after 30 seconds
    
    Changes made:
    - Updated gradle/libs.versions.toml to use PipePipeExtractor version 4240401 from InfinityLoop1308
    - Updated NewPipe.kt to support new Response constructor with byte array parameter
    - Added CancellableCall import and executeAsync method placeholder for compatibility
    
    PipePipe Extractor is a fork of NewPipe Extractor that provides better
    deobfuscation support and resolves issues with broken WEB_REMIX and
    TVHTML5_SIMPLY_EMBEDDED_PLAYER clients that fallback to IOS client
    which gets blocked by YouTube.
    
    References:
    - Original PR: https://github.com/OuterTune/OuterTune/pull/730
    - Original repository: https://github.com/OuterTune/OuterTune
    - PipePipe Extractor: https://github.com/InfinityLoop1308/PipePipeExtractor
    - Related issue: https://github.com/OuterTune/OuterTune/issues/468
    
    Credit: @DanielProg39 for the original implementation

--- innertube/src/main/kotlin/com/metrolist/innertube/pages/NewPipe.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/pages/NewPipe.kt
@@ -7,6 +7,7 @@ import io.ktor.http.parseQueryString
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.schabi.newpipe.extractor.NewPipe
+import org.schabi.newpipe.extractor.downloader.CancellableCall
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.downloader.Request
 import org.schabi.newpipe.extractor.downloader.Response
@@ -56,7 +57,11 @@ private class NewPipeDownloaderImpl(proxy: Proxy?) : Downloader() {
         val responseBodyToReturn = response.body?.string()
 
         val latestUrl = response.request.url.toString()
-        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, latestUrl)
+        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, responseBodyToReturn?.toByteArray(), latestUrl)
+    }
+
+    override fun executeAsync(request: Request, callback: AsyncCallback?): CancellableCall {
+        TODO("Placeholder")
     }
 
 }
