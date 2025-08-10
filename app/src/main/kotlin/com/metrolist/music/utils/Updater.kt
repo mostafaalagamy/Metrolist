ec5717d05a0679c2629359afc137a3dfc4a3d029
Sumit Mohanty <s8m1tx+github@gmail.com>
2025-08-10 15:27:41 +0530

    Use ABI-aware download URL for in-app updates

--- app/src/main/kotlin/com/metrolist/music/utils/Updater.kt
+++ app/src/main/kotlin/com/metrolist/music/utils/Updater.kt
@@ -1,5 +1,6 @@
 package com.metrolist.music.utils
 
+import com.metrolist.music.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
@@ -20,4 +21,14 @@ object Updater {
             lastCheckTime = System.currentTimeMillis()
             versionName
         }
+
+    fun getLatestDownloadUrl(): String {
+        val baseUrl = "https://github.com/mostafaalagamy/Metrolist/releases/latest/download/"
+        val architecture = BuildConfig.ARCHITECTURE
+        return if (architecture == "universal") {
+            baseUrl + "Metrolist.apk"
+        } else {
+            baseUrl + "app-${architecture}-release.apk"
+        }
+    }
 }
