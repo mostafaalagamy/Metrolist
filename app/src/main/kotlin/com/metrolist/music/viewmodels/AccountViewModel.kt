62dab8a27031ecbd6d63d608c81ab3f6ec39b46f
Damian Sobczak <sobczakd2017@gmail.com>
2025-09-10 15:33:28 +0200

    fix: improve thumbnails quality in some screens
    Closes #1078
    
    ---------
    
    Co-authored-by: MO AGAMY <mostafaalagamy@email.com>

--- app/src/main/kotlin/com/metrolist/music/viewmodels/AccountViewModel.kt
+++ app/src/main/kotlin/com/metrolist/music/viewmodels/AccountViewModel.kt
@@ -8,6 +8,7 @@ import com.metrolist.innertube.models.ArtistItem
 import com.metrolist.innertube.models.PlaylistItem
 import com.metrolist.innertube.utils.completed
 import com.metrolist.music.utils.reportException
+import com.metrolist.music.ui.utils.resize
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -40,7 +41,11 @@ class AccountViewModel @Inject constructor() : ViewModel() {
                 reportException(it)
             }
             YouTube.library("FEmusic_library_corpus_artists").completed().onSuccess {
-                artists.value = it.items.filterIsInstance<ArtistItem>()
+                artists.value = it.items.filterIsInstance<ArtistItem>().map { artist ->
+                    artist.copy(
+                        thumbnail = artist.thumbnail?.resize(544, 544)
+                    )
+                }
             }.onFailure {
                 reportException(it)
             }
