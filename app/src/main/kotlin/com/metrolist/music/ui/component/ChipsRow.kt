c46f93d098085f083e46cf4d8c790676b78d2df5
MO AGAMY <mostafaalagamy@email.com>
2025-09-17 03:22:49 +0300

    I always forget this

--- app/src/main/kotlin/com/metrolist/music/ui/component/ChipsRow.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/ChipsRow.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
