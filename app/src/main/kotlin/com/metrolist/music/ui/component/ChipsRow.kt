e6ba92acf19c7537aa0e1365c05db51e1ad99c9e
mostafaalagamy <mostafaalagamy@email.com>
2025-09-17 00:15:57 +0000

    Add contentPadding to ChipsRow and SuggestionItem components for proper safe area handling

--- app/src/main/kotlin/com/metrolist/music/ui/component/ChipsRow.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/ChipsRow.kt
@@ -20,6 +20,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
