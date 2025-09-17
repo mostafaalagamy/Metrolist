43031c1d811229dfcaebe13073a5761e1e41246e
MO AGAMY <mostafaalagamy@email.com>
2025-09-17 03:58:30 +0300

    Add only damn

--- app/src/main/kotlin/com/metrolist/music/ui/screens/AlbumScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/AlbumScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
