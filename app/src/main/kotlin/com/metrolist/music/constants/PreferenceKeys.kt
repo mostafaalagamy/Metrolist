0ba9b5d2cd4e30d1d9503d6a9080d7b7b5d8a4d8
itsTeddy <itsmeteddyhere@proton.me>
2025-09-23 00:49:35 +0200

    feat: add macedonian romanization (#1569)
    Close #1562

--- app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
@@ -289,6 +289,7 @@ val LyricsRomanizeSerbianKey = booleanPreferencesKey("lyricsRomanizeSerbian")
 val LyricsRomanizeBulgarianKey = booleanPreferencesKey("lyricsRomanizeBulgarian")
 val LyricsRomanizeBelarusianKey = booleanPreferencesKey("lyricsRomanizeBelarusian")
 val LyricsRomanizeKyrgyzKey = booleanPreferencesKey("lyricsRomanizeKyrgyz")
+val LyricsRomanizeMacedonianKey = booleanPreferencesKey("lyricsRomanizeMacedonian")
 val LyricsRomanizeCyrillicByLineKey = booleanPreferencesKey("lyricsRomanizeCyrillicByLine")
 val TranslateLyricsKey = booleanPreferencesKey("translateLyrics")
 
