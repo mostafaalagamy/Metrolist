ad277b66058e118376a3ea54360dfdadbdae7dc2
MO AGAMY <mostafaalagamy@email.com>
2025-09-23 19:46:20 +0300

    fix(build): resolve compile errors

--- app/src/main/kotlin/com/metrolist/music/MainActivity.kt
+++ app/src/main/kotlin/com/metrolist/music/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
