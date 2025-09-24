da4a97c33ce406cf5573ac006a8d1e3f5a812959
MO AGAMY <mostafaalagamy@email.com>
2025-09-24 18:28:42 +0300

    Thumbnail.kt

--- app/src/main/kotlin/com/metrolist/music/ui/player/Thumbnail.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/player/Thumbnail.kt
@@ -344,7 +344,7 @@ fun Thumbnail(
                                             contentAlignment = Alignment.Center
                                         ) {
                                             Icon(
-                                                painter = painterResource(R.drawable.about_splash),
+                                                painter = painterResource(R.drawable.small_icon),
                                                 contentDescription = stringResource(R.string.hide_player_thumbnail),
                                                 tint = textBackgroundColor.copy(alpha = 0.7f),
                                                 modifier = Modifier.size(120.dp)
