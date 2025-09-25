8547982937abf269b2e29e8441d97d6056b959be
MO AGAMY <mostafaalagamy@email.com>
2025-09-25 18:15:56 +0300

    refactor(YouTubeClient): add new YouTube Clients

--- innertube/src/main/kotlin/com/metrolist/innertube/models/YouTubeClient.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/models/YouTubeClient.kt
@@ -8,7 +8,15 @@ data class YouTubeClient(
     val clientVersion: String,
     val clientId: String,
     val userAgent: String,
+    val osName: String? = null,
     val osVersion: String? = null,
+    val deviceMake: String? = null,
+    val deviceModel: String? = null,
+    val androidSdkVersion: String? = null,
+    val buildId: String? = null,
+    val cronetVersion: String? = null,
+    val packageName: String? = null,
+    val friendlyName: String? = null,
     val loginSupported: Boolean = false,
     val loginRequired: Boolean = false,
     val useSignatureTimestamp: Boolean = false,
@@ -20,7 +28,11 @@ data class YouTubeClient(
         client = Context.Client(
             clientName = clientName,
             clientVersion = clientVersion,
+            osName = osName,
             osVersion = osVersion,
+            deviceMake = deviceMake,
+            deviceModel = deviceModel,
+            androidSdkVersion = androidSdkVersion,
             gl = locale.gl,
             hl = locale.hl,
             visitorData = visitorData
@@ -75,7 +87,7 @@ data class YouTubeClient(
             loginRequired = true,
             useSignatureTimestamp = true
         )
-        
+
         val TVHTML5_SIMPLY_EMBEDDED_PLAYER = YouTubeClient(
             clientName = "TVHTML5_SIMPLY_EMBEDDED_PLAYER",
             clientVersion = "2.0",
@@ -111,6 +123,118 @@ data class YouTubeClient(
             userAgent = "com.google.android.apps.youtube.vr.oculus/1.61.48 (Linux; U; Android 12; en_US; Oculus Quest 3; Build/SQ3A.220605.009.A1; Cronet/132.0.6808.3)",
             loginSupported = false,
             useSignatureTimestamp = false
+        )
+
+        /**
+         * Video not playable: Kids / Paid / Movie / Private / Age-restricted.
+         * This client can only be used when logged out.
+         */
+        // https://dumps.tadiphone.dev/dumps/oculus/eureka
+        val ANDROID_VR_1_61_48 = YouTubeClient(
+            clientName = "ANDROID_VR",
+            clientVersion = "1.61.48",
+            clientId = "28",
+            userAgent = "com.google.android.apps.youtube.vr.oculus/1.61.48 (Linux; U; Android 12; en_US; Quest 3; Build/SQ3A.220605.009.A1; Cronet/132.0.6808.3)",
+            osName = "Android",
+            osVersion = "12",
+            deviceMake = "Oculus",
+            deviceModel = "Quest 3",
+            androidSdkVersion = "32",
+            buildId = "SQ3A.220605.009.A1",
+            cronetVersion = "132.0.6808.3",
+            packageName = "com.google.android.apps.youtube.vr.oculus",
+            friendlyName = "Android VR 1.61",
+            loginSupported = false,
+            useSignatureTimestamp = false
+        )
+
+        /**
+         * Uses non adaptive bitrate, which fixes audio stuttering with YT Music.
+         * Does not use AV1.
+         */
+        val ANDROID_VR_1_43_32 = YouTubeClient(
+            clientName = "ANDROID_VR",
+            clientVersion = "1.43.32",
+            clientId = "28",
+            userAgent = "com.google.android.apps.youtube.vr.oculus/1.43.32 (Linux; U; Android 12; en_US; Quest 3; Build/SQ3A.220605.009.A1; Cronet/107.0.5284.2)",
+            osName = "Android",
+            osVersion = "12",
+            deviceMake = "Oculus",
+            deviceModel = "Quest 3",
+            androidSdkVersion = "32",
+            buildId = "SQ3A.220605.009.A1",
+            cronetVersion = "107.0.5284.2",
+            packageName = "com.google.android.apps.youtube.vr.oculus",
+            friendlyName = "Android VR 1.43",
+            loginSupported = false,
+            useSignatureTimestamp = false
+        )
+
+        /**
+         * Cannot play livestreams and lacks HDR, but can play videos with music and labeled "for children".
+         * <a href=\"https://dumps.tadiphone.dev/dumps/google/barbet\">Google Pixel 9 Pro Fold</a>
+         */
+        val ANDROID_CREATOR = YouTubeClient(
+            clientName = "ANDROID_CREATOR",
+            clientVersion = "23.47.101",
+            clientId = "14",
+            userAgent = "com.google.android.apps.youtube.creator/23.47.101 (Linux; U; Android 15; en_US; Pixel 9 Pro Fold; Build/AP3A.241005.015.A2; Cronet/132.0.6779.0)",
+            osName = "Android",
+            osVersion = "15",
+            deviceMake = "Google",
+            deviceModel = "Pixel 9 Pro Fold",
+            androidSdkVersion = "35",
+            buildId = "AP3A.241005.015.A2",
+            cronetVersion = "132.0.6779.0",
+            packageName = "com.google.android.apps.youtube.creator",
+            friendlyName = "Android Studio",
+            loginSupported = true,
+            useSignatureTimestamp = true
+        )
+
+        /**
+         * Internal YT client for an unreleased YT client. May stop working at any time.
+         */
+        val VISIONOS = YouTubeClient(
+            clientName = "VISIONOS",
+            clientVersion = "0.1",
+            clientId = "101",
+            userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Safari/605.1.15",
+            osName = "visionOS",
+            osVersion = "1.3.21O771",
+            deviceMake = "Apple",
+            deviceModel = "RealityDevice14,1",
+            friendlyName = "visionOS",
+            loginSupported = false,
+            useSignatureTimestamp = false
+        )
+
+        /**
+         * The device machine id for the iPad 6th Gen (iPad7,6).
+         * AV1 hardware decoding is not supported.
+         * See [this GitHub Gist](https://gist.github.com/adamawolf/3048717) for more information.
+         *
+         * Based on Google's actions to date, PoToken may not be required on devices with very low specs.
+         * For example, suppose the User-Agent for a PlayStation 3 (with 256MB of RAM) is used.
+         * Accessing 'Web' (https://www.youtube.com) will redirect to 'TV' (https://www.youtube.com/tv).
+         * 'TV' target devices with very low specs, such as embedded devices, game consoles, and blu-ray players, so PoToken is not required.
+         *
+         * For this reason, the device machine id for the iPad 6th Gen (with 2GB of RAM),
+         * the lowest spec device capable of running iPadOS 17, was used.
+         */
+        val IPADOS = YouTubeClient(
+            clientName = "IOS",
+            clientVersion = "19.22.3",
+            clientId = "5",
+            userAgent = "com.google.ios.youtube/19.22.3 (iPad7,6; U; CPU iPadOS 17_7_10 like Mac OS X; en-US)",
+            osName = "iPadOS",
+            osVersion = "17.7.10.21H450",
+            deviceMake = "Apple",
+            deviceModel = "iPad7,6",
+            friendlyName = "iPadOS",
+            loginSupported = false,
+            useSignatureTimestamp = false,
+            packageName = "com.google.ios.youtube"
         )
     }
 }
