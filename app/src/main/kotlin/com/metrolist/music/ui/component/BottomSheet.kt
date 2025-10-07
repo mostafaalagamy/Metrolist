88a2fe206f7183937762dff6ed55fcb3a59261d2
MO AGAMY <mostafaalagamy@email.com>
2025-10-07 04:32:20 +0300

    BottomSheet.kt

--- app/src/main/kotlin/com/metrolist/music/ui/component/BottomSheet.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/component/BottomSheet.kt
@@ -65,6 +65,15 @@ fun BottomSheet(
 ) {
     Box(
         modifier = modifier
+            .graphicsLayer {
+                // background fades during about 10%-61% progress
+                alpha = (1.4f * (state.progress.coerceAtLeast(0.1f) - 0.1f).pow(0.5f)).coerceIn(0f, 1f)
+            }
+            .fillMaxSize(),
+        content = backgroundColor
+    )
+    Box(
+        modifier = modifier
             .fillMaxSize()
             .offset {
                 val y = (state.expandedBound - state.value)
@@ -98,8 +107,6 @@ fun BottomSheet(
                 )
             )
     ) {
-        backgroundColor()
-
         if (!state.isCollapsed && !state.isDismissed) {
             BackHandler(onBack = state::collapseSoft)
         }
