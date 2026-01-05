7dea6ccd1b68f04aae81e7a4d586246e462a1f9b
MO AGAMY <mostafaalagamy@email.com>
2026-01-05 06:49:01 +0200

    Refactor navigation builder and clean imports

--- app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
@@ -5,6 +5,7 @@
 
 package com.metrolist.music.ui.screens
 
+import android.app.Activity
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -12,6 +13,7 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -23,7 +25,7 @@ import androidx.navigation.compose.dialog
 import androidx.navigation.navArgument
 import com.metrolist.music.constants.DarkModeKey
 import com.metrolist.music.constants.PureBlackKey
-import com.metrolist.music.utils.rememberEnumPreference
+import com.metrolist.music.eq.data.EQProfileRepository
 import com.metrolist.music.ui.screens.artist.ArtistAlbumsScreen
 import com.metrolist.music.ui.screens.artist.ArtistItemsScreen
 import com.metrolist.music.ui.screens.artist.ArtistScreen
@@ -31,10 +33,10 @@ import com.metrolist.music.ui.screens.artist.ArtistSongsScreen
 import com.metrolist.music.ui.screens.equalizer.EqScreen
 import com.metrolist.music.ui.screens.library.LibraryScreen
 import com.metrolist.music.ui.screens.playlist.AutoPlaylistScreen
+import com.metrolist.music.ui.screens.playlist.CachePlaylistScreen
 import com.metrolist.music.ui.screens.playlist.LocalPlaylistScreen
 import com.metrolist.music.ui.screens.playlist.OnlinePlaylistScreen
 import com.metrolist.music.ui.screens.playlist.TopPlaylistScreen
-import com.metrolist.music.ui.screens.playlist.CachePlaylistScreen
 import com.metrolist.music.ui.screens.search.OnlineSearchResult
 import com.metrolist.music.ui.screens.search.SearchScreen
 import com.metrolist.music.ui.screens.settings.AboutScreen
@@ -44,20 +46,18 @@ import com.metrolist.music.ui.screens.settings.BackupAndRestore
 import com.metrolist.music.ui.screens.settings.ContentSettings
 import com.metrolist.music.ui.screens.settings.DarkMode
 import com.metrolist.music.ui.screens.settings.DiscordLoginScreen
-import com.metrolist.music.ui.screens.settings.integrations.DiscordSettings
-import com.metrolist.music.ui.screens.settings.integrations.IntegrationScreen
-import com.metrolist.music.ui.screens.settings.integrations.LastFMSettings
 import com.metrolist.music.ui.screens.settings.PlayerSettings
 import com.metrolist.music.ui.screens.settings.PrivacySettings
 import com.metrolist.music.ui.screens.settings.RomanizationSettings
 import com.metrolist.music.ui.screens.settings.SettingsScreen
 import com.metrolist.music.ui.screens.settings.StorageSettings
 import com.metrolist.music.ui.screens.settings.UpdaterScreen
+import com.metrolist.music.ui.screens.settings.integrations.DiscordSettings
+import com.metrolist.music.ui.screens.settings.integrations.IntegrationScreen
+import com.metrolist.music.ui.screens.settings.integrations.LastFMSettings
 import com.metrolist.music.ui.screens.wrapped.WrappedScreen
+import com.metrolist.music.utils.rememberEnumPreference
 import com.metrolist.music.utils.rememberPreference
-import android.app.Activity
-import androidx.compose.material3.SnackbarHostState
-import com.metrolist.music.eq.data.EQProfileRepository
 
 @OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.navigationBuilder(
@@ -70,6 +70,7 @@ fun NavGraphBuilder.navigationBuilder(
     composable(Screens.Home.route) {
         HomeScreen(navController = navController, snackbarHostState = snackbarHostState)
     }
+
     composable(Screens.Search.route) {
         val pureBlackEnabled by rememberPreference(PureBlackKey, defaultValue = false)
         val darkTheme by rememberEnumPreference(DarkModeKey, defaultValue = DarkMode.AUTO)
@@ -85,29 +86,35 @@ fun NavGraphBuilder.navigationBuilder(
             pureBlack = pureBlack
         )
     }
-    composable(
-        Screens.Library.route,
-    ) {
+
+    composable(Screens.Library.route) {
         LibraryScreen(navController)
     }
+
     composable("history") {
         HistoryScreen(navController)
     }
+
     composable("stats") {
         StatsScreen(navController)
     }
+
     composable("mood_and_genres") {
         MoodAndGenresScreen(navController, scrollBehavior)
     }
+
     composable("account") {
         AccountScreen(navController, scrollBehavior)
     }
+
     composable("new_release") {
         NewReleaseScreen(navController, scrollBehavior)
     }
+
     composable("charts_screen") {
         ChartsScreen(navController)
     }
+
     composable(
         route = "browse/{browseId}",
         arguments = listOf(
@@ -122,14 +129,14 @@ fun NavGraphBuilder.navigationBuilder(
             it.arguments?.getString("browseId")
         )
     }
+
     composable(
         route = "search/{query}",
-        arguments =
-            listOf(
-                navArgument("query") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("query") {
+                type = NavType.StringType
+            },
+        ),
         enterTransition = {
             fadeIn(tween(250))
         },
@@ -153,39 +160,40 @@ fun NavGraphBuilder.navigationBuilder(
     ) {
         OnlineSearchResult(navController)
     }
+
     composable(
         route = "album/{albumId}",
-        arguments =
-            listOf(
-                navArgument("albumId") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("albumId") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         AlbumScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "artist/{artistId}",
-        arguments =
-            listOf(
-                navArgument("artistId") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("artistId") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         ArtistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "artist/{artistId}/songs",
-        arguments =
-            listOf(
-                navArgument("artistId") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("artistId") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         ArtistSongsScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "artist/{artistId}/albums",
         arguments = listOf(
@@ -196,144 +204,161 @@ fun NavGraphBuilder.navigationBuilder(
     ) {
         ArtistAlbumsScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "artist/{artistId}/items?browseId={browseId}?params={params}",
-        arguments =
-            listOf(
-                navArgument("artistId") {
-                    type = NavType.StringType
-                },
-                navArgument("browseId") {
-                    type = NavType.StringType
-                    nullable = true
-                },
-                navArgument("params") {
-                    type = NavType.StringType
-                    nullable = true
-                },
-            ),
+        arguments = listOf(
+            navArgument("artistId") {
+                type = NavType.StringType
+            },
+            navArgument("browseId") {
+                type = NavType.StringType
+                nullable = true
+            },
+            navArgument("params") {
+                type = NavType.StringType
+                nullable = true
+            },
+        ),
     ) {
         ArtistItemsScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "online_playlist/{playlistId}",
-        arguments =
-            listOf(
-                navArgument("playlistId") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("playlistId") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         OnlinePlaylistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "local_playlist/{playlistId}",
-        arguments =
-            listOf(
-                navArgument("playlistId") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("playlistId") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         LocalPlaylistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "auto_playlist/{playlist}",
-        arguments =
-            listOf(
-                navArgument("playlist") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("playlist") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         AutoPlaylistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "cache_playlist/{playlist}",
-        arguments =
-            listOf(
-                navArgument("playlist") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("playlist") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         CachePlaylistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "top_playlist/{top}",
-        arguments =
-            listOf(
-                navArgument("top") {
-                    type = NavType.StringType
-                },
-            ),
+        arguments = listOf(
+            navArgument("top") {
+                type = NavType.StringType
+            },
+        ),
     ) {
         TopPlaylistScreen(navController, scrollBehavior)
     }
+
     composable(
         route = "youtube_browse/{browseId}?params={params}",
-        arguments =
-            listOf(
-                navArgument("browseId") {
-                    type = NavType.StringType
-                    nullable = true
-                },
-                navArgument("params") {
-                    type = NavType.StringType
-                    nullable = true
-                },
-            ),
+        arguments = listOf(
+            navArgument("browseId") {
+                type = NavType.StringType
+                nullable = true
+            },
+            navArgument("params") {
+                type = NavType.StringType
+                nullable = true
+            },
+        ),
     ) {
         YouTubeBrowseScreen(navController)
     }
+
     composable("settings") {
         SettingsScreen(navController, scrollBehavior, latestVersionName)
     }
+
     composable("settings/appearance") {
         AppearanceSettings(navController, scrollBehavior, activity, snackbarHostState)
     }
+
     composable("settings/content") {
         ContentSettings(navController, scrollBehavior)
     }
+
     composable("settings/content/romanization") {
         RomanizationSettings(navController, scrollBehavior)
     }
+
     composable("settings/player") {
         PlayerSettings(navController, scrollBehavior)
     }
+
     composable("settings/storage") {
         StorageSettings(navController, scrollBehavior)
     }
+
     composable("settings/privacy") {
         PrivacySettings(navController, scrollBehavior)
     }
+
     composable("settings/backup_restore") {
         BackupAndRestore(navController, scrollBehavior)
     }
+
     composable("settings/integrations") {
         IntegrationScreen(navController, scrollBehavior)
     }
+
     composable("settings/integrations/discord") {
         DiscordSettings(navController, scrollBehavior)
     }
+
     composable("settings/integrations/lastfm") {
         LastFMSettings(navController, scrollBehavior)
     }
+
     composable("settings/discord/login") {
         DiscordLoginScreen(navController)
     }
+
     composable("settings/updater") {
         UpdaterScreen(navController, scrollBehavior)
     }
+
     composable("settings/about") {
         AboutScreen(navController, scrollBehavior)
     }
+
     composable("login") {
         LoginScreen(navController)
     }
+
     composable("wrapped") {
         WrappedScreen(navController)
     }
+
     dialog("equalizer") {
         EqScreen()
     }
