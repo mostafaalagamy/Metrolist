6944c4fdfe94a9e868dce98851397d5a62eaee7a
MO AGAMY <mostafaalagamy@email.com>
2026-01-11 19:59:27 +0200

    feat: Redesign Music and Turntable widgets
    - Add new Music Widget with Material 3 Expressive design
    - Add Turntable Widget (2x2) with circular album art

---
+++ app/src/main/kotlin/com/metrolist/music/widget/MusicWidgetActions.kt
@@ -0,0 +1,19 @@
+/**
+ * Metrolist Project (C) 2026
+ * Licensed under GPL-3.0 | See git history for contributors
+ */
+
+package com.metrolist.music.widget
+
+/**
+ * Shared action constants for widget controls.
+ * Used by both legacy RemoteViews widget and Glance widget.
+ */
+object MusicWidgetActions {
+    const val ACTION_PLAY_PAUSE = "com.metrolist.music.ACTION_PLAY"
+    const val ACTION_NEXT = "com.metrolist.music.ACTION_NEXT"
+    const val ACTION_PREV = "com.metrolist.music.ACTION_PREV"
+    const val ACTION_LIKE = "com.metrolist.music.ACTION_LIKE"
+    const val ACTION_SHARE = "com.metrolist.music.ACTION_SHARE"
+    const val ACTION_UPDATE_WIDGET = "com.metrolist.music.ACTION_UPDATE_WIDGET"
+}
