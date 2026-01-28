0977a6b90858c11af029137ff9480aabdb50e1fb
mostafaalagamy <mostafaalagamy@email.com>
2026-01-28 16:28:37 +0000

    fix: Update NewPipeExtractor to match official API
    - Removed CancellableCall and AsyncCallback (not in official API)
    - Fixed Response constructor to match official signature
    - Removed executeAsync method override

--- innertube/src/main/kotlin/com/metrolist/innertube/pages/NewPipeExtractor.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/pages/NewPipeExtractor.kt
@@ -7,7 +7,6 @@ import io.ktor.http.parseQueryString
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.schabi.newpipe.extractor.NewPipe as OfficialNewPipe
-import org.schabi.newpipe.extractor.downloader.CancellableCall
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.downloader.Request
 import org.schabi.newpipe.extractor.downloader.Response
@@ -66,11 +65,7 @@ private class NewPipeExtractorDownloaderImpl(proxy: Proxy?, proxyAuth: String?) 
 
         val responseBodyToReturn = response.body.string()
         val latestUrl = response.request.url.toString()
-        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, responseBodyToReturn.toByteArray(), latestUrl)
-    }
-
-    override fun executeAsync(request: Request, callback: AsyncCallback?): CancellableCall {
-        TODO("Placeholder")
+        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, latestUrl)
     }
 }
 
