5dc674ec0ac06fadabcada2fb60c88b19e65d4ae
Kai Longmore <14970686+klongmore@users.noreply.github.com>
2026-02-04 12:34:07 +1100

    feat: Implement a working playlist shuffle for Android Auto, close #830

--- app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
+++ app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
@@ -2529,6 +2529,7 @@ class MusicService :
         const val PLAYLIST = "playlist"
         const val YOUTUBE_PLAYLIST = "youtube_playlist"
         const val SEARCH = "search"
+        const val SHUFFLE_ACTION = "__shuffle__"
 
         const val CHANNEL_ID = "music_channel_01"
         const val NOTIFICATION_ID = 888
