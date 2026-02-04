14d2fe8885f832c6c1ef4dd96e7dbd60993de78d
Adriel <adriel.gg.motion@proton.me>
2026-02-03 22:31:10 -0300

    Feat: Add crash handler with crash screen

--- app/src/main/kotlin/com/metrolist/music/App.kt
+++ app/src/main/kotlin/com/metrolist/music/App.kt
@@ -30,6 +30,7 @@ import com.metrolist.music.constants.*
 import com.metrolist.music.di.ApplicationScope
 import com.metrolist.music.extensions.toEnum
 import com.metrolist.music.extensions.toInetSocketAddress
+import com.metrolist.music.utils.CrashHandler
 import com.metrolist.music.utils.dataStore
 import com.metrolist.music.utils.get
 import com.metrolist.music.utils.reportException
@@ -59,6 +60,10 @@ class App : Application(), SingletonImageLoader.Factory {
 
     override fun onCreate() {
         super.onCreate()
+        
+        // Install crash handler first
+        CrashHandler.install(this)
+        
         Timber.plant(Timber.DebugTree())
 
         // تهيئة إعدادات التطبيق عند الإقلاع
